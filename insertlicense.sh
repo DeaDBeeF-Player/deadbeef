e961015695b5c65c61a2b945989c38061374e0a7
waker <wakeroid@gmail.com>
2009-08-25 19:48:11 +0200

    shielded deadbeef.h from GPL

--- insertlicense.sh
+++ insertlicense.sh
@@ -2,7 +2,7 @@
 echo "IMPORTANT: Hit C-c if you forgot to backup/commit stuff!!!"
 read
 ls *.c *.h *.cpp | while read i; do
-    if [ "$i" == "config.h" ] || grep --silent "DO NOT EDIT" "$i" ; then
+    if [ "$i" == "deadbeef.h" ] || [ "$i" == "config.h" ] || grep --silent "DO NOT EDIT" "$i" ; then
         echo "skipping $i (blacklist)"
     elif grep --silent "DeaDBeeF - ultimate music player for GNU/Linux systems with X11" "$i" ; then
         if grep --silent "version 3 of the License" "$i" ; then
