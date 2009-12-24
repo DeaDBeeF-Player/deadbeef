2cf8bccda9d7b86617c8b7f65a6d1c0f5ae9b8a8
Alexey Yakovenko <wakeroid@gmail.com>
2009-12-24 18:25:40 +0100

    undone sid config change from last commit

--- csid.cpp
+++ csid.cpp
@@ -55,7 +55,6 @@ const char *filetypes[] = { "SID", NULL };
 static const char settings_dlg[] =
     "property \"Enable HVSC\" checkbox hvsc_enable 0;\n"
     "property \"HVSC path\" file hvsc_path \"\";\n"
-    "property \"Synth samplerate\" entry sid.samplerate 48000;\n"
 ;
 
 // define plugin interface
