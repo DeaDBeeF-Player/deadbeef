21bbd790c4e461e88cbc0a39fac971b7784e6920
Alexey Yakovenko <wakeroid@gmail.com>
2010-01-29 22:08:11 +0100

    few fixes to plugin configuration dialog

--- plugins/gtkui/pluginconf.c
+++ plugins/gtkui/pluginconf.c
@@ -126,12 +126,12 @@ plugin_configure (GtkWidget *parentwin, DB_plugin_t *p) {
     gtk_window_set_type_hint (GTK_WINDOW (win), GDK_WINDOW_TYPE_HINT_DIALOG);
     gtk_container_set_border_width (GTK_CONTAINER(win), 12);
 
-    gtk_widget_set_size_request (win, 300, -1);
     gtk_window_set_title (GTK_WINDOW (win), title);
     gtk_window_set_modal (GTK_WINDOW (win), TRUE);
     gtk_window_set_transient_for (GTK_WINDOW (win), GTK_WINDOW (parentwin));
     GtkWidget *vbox;
     vbox = GTK_DIALOG (win)->vbox;
+    gtk_box_set_spacing (GTK_BOX (vbox), 8);
     GtkWidget *action_area = GTK_DIALOG (win)->action_area;
     gtk_widget_show (action_area);
     gtk_button_box_set_layout (GTK_BUTTON_BOX (action_area), GTK_BUTTONBOX_END);
