68ba7966ebaff30646c34c5007b08631f4c1c0e4
Alexey Yakovenko <wakeroid@gmail.com>
2010-05-16 19:16:42 +0200

    fixed gettext support in playlist.c

--- playlist.c
+++ playlist.c
@@ -15,9 +15,9 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-//#ifdef HAVE_CONFIG_H
-//#  include <config.h>
-//#endif
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
