dd0748eaf2acf9c297d4e997ba85c22fc343a711
Alexey Yakovenko <wakeroid@gmail.com>
2010-06-15 19:53:10 +0200

    explicitly set cairo line width when drawing seekbar

--- plugins/gtkui/callbacks.c
+++ plugins/gtkui/callbacks.c
@@ -435,9 +435,10 @@ seekbar_draw (GtkWidget *widget) {
         if (trk) {
             deadbeef->pl_item_unref (trk);
         }
-        clearlooks_rounded_rectangle (cr, 2+ax, widget->allocation.height/2-4+ay, aw-4, 8, 4, 0xff);
         // empty seekbar, just a frame
+        clearlooks_rounded_rectangle (cr, 2+ax, widget->allocation.height/2-4+ay, aw-4, 8, 4, 0xff);
         cairo_set_source_rgb (cr, clr_selection.red/65535.f, clr_selection.green/65535.f, clr_selection.blue/65535.f );
+        cairo_set_line_width (cr, 2);
         cairo_stroke (cr);
         cairo_destroy (cr);
         return;
