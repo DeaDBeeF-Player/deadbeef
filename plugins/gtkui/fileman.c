98ebd884f7646bfa7d3fea3604e165f04ca61ebb
Alexey Yakovenko <wakeroid@gmail.com>
2010-08-29 14:58:09 +0200

    not resuming paused track anymore after "open file(s)"

--- plugins/gtkui/fileman.c
+++ plugins/gtkui/fileman.c
@@ -103,7 +103,7 @@ open_files_worker (void *data) {
     extern GtkWidget *mainwin;
     DdbListview *pl = DDB_LISTVIEW (lookup_widget (mainwin, "playlist"));
     ddb_listview_set_cursor (pl, 0);
-    deadbeef->sendmessage (M_PLAYSONG, 0, 0, 0);
+    deadbeef->sendmessage (M_PLAYSONG, 0, 1, 0);
 }
 
 void
