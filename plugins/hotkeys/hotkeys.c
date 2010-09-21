ab250b681ea1148abc30359fc2d19c651b7c6dd5
Alexey Yakovenko <wakeroid@gmail.com>
2010-09-21 20:22:45 +0200

    disabled hotkeys tracing

--- plugins/hotkeys/hotkeys.c
+++ plugins/hotkeys/hotkeys.c
@@ -25,8 +25,8 @@
 #include "hotkeys.h"
 #include "../../deadbeef.h"
 
-#define trace(...) { fprintf(stderr, __VA_ARGS__); }
-//#define trace(fmt,...)
+//#define trace(...) { fprintf(stderr, __VA_ARGS__); }
+#define trace(fmt,...)
 
 static DB_hotkeys_plugin_t plugin;
 static DB_functions_t *deadbeef;
