b28f9aea5b681ec3c946ab6933629e2d81fbcfba
Dmitriy Simbiriatin <slpiv@mail.ru>
2010-10-01 20:19:13 +0300

    added the new strings in the extra.c file, updated the deadbeef.pot file, updated the russian translation file, fixed some mistakes in the help.ru.txt file

--- translation/extra.c
+++ translation/extra.c
@@ -2,3 +2,52 @@
 // directly in source code, e.g. scripted plugin configuration strings
 _("Add Audio CD")
 _("Lookup on Last.fm")
+// ALSA output plugin
+_("Use ALSA resampling")
+_("Release device while stopped")
+_("Preferred buffer size")
+_("Preferred period size")
+// Last.fm plugin
+_("Enable scrobbler")
+_("Disable nowplaying")
+_("Username")
+_("Password")
+_("Scrobble URL")
+// Album Artwork plugin
+_("Cache update period (hr)")
+_("Fetch from embedded tags")
+_("Fetch from local folder")
+_("Local cover file mask")
+_("Fetch from last.fm")
+_("Fetch from albumart.org")
+// Audio CD player
+_("Use CDDB/FreeDB")
+_("Prefer CD-Text over CDDB")
+_("CDDB url (e.g. 'freedb.org')")
+_("CDDB port number (e.g. '888')")
+_("Prefer CDDB protocol over HTTP")
+_("Enable NRG image support")
+// DUMB module player plugin
+_("Resampling quality (0..2, higher is better)")
+// Game_Music_Emu decoder plugin
+_("Max song length (in minutes)")
+// Standard GTK2 user interface plugin
+_("Ask confirmation to delete files from disk")
+_("Status icon volume control sensitivity")
+_("Custom status icon")
+_("Run gtk_init with --sync (debug mode)")
+// OSD Notify plugin
+_("Notification format")
+// PulseAudio output plugin
+_("PulseAudio server")
+_("Preferred buffer size")
+_("Samplerate")
+// SHN player plugin
+_("Relative seek table path")
+_("Absolute seek table path")
+_("Swap audio bytes (toggle if all you hear is static)")
+// SID decoder plugin
+_("Enable HVSC")
+_("HVSC path")
+// WildMidi player plugin
+_("Timidity++ bank configuration file")
