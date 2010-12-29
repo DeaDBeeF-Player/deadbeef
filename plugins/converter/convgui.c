3ff49514df9f75424bffd58a0e87dcfe4b266f2c
waker <wakeroid@gmail.com>
2010-12-29 12:38:31 +0100

    removed ellipsis from Convert action name

--- plugins/converter/convgui.c
+++ plugins/converter/convgui.c
@@ -1014,7 +1014,7 @@ encoder_cmdline_help_link_create (gchar *widget_name, gchar *string1, gchar *str
 }
 
 static DB_plugin_action_t convert_action = {
-    .title = "Convert...",
+    .title = "Convert",
     .name = "convert",
     .flags = DB_ACTION_CAN_MULTIPLE_TRACKS | DB_ACTION_ALLOW_MULTIPLE_TRACKS | DB_ACTION_SINGLE_TRACK,
     .callback = converter_show,
