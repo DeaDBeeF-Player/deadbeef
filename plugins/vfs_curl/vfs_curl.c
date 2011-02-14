03f148d17f82c53b982f5213a0caa48277f9d5dc
waker <wakeroid@gmail.com>
2011-02-14 22:57:31 +0100

    disabled vfs_curl tracing

--- plugins/vfs_curl/vfs_curl.c
+++ plugins/vfs_curl/vfs_curl.c
@@ -25,8 +25,8 @@
 #include <time.h>
 #include "../../deadbeef.h"
 
-#define trace(...) { fprintf(stderr, __VA_ARGS__); }
-//#define trace(fmt,...)
+//#define trace(...) { fprintf(stderr, __VA_ARGS__); }
+#define trace(fmt,...)
 
 #define min(x,y) ((x)<(y)?(x):(y))
 #define max(x,y) ((x)>(y)?(x):(y))
