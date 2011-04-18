d80680ca66409df7bef4ff032e1b6d309f6855b4
waker <wakeroid@gmail.com>
2011-04-18 22:10:01 +0200

    0.5.0-beta1 build

---
+++ scripts/portable_package_static.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# package for distribution
+VERSION=`cat PORTABLE_VERSION | perl -ne 'chomp and print'`
+BUILD=`cat PORTABLE_BUILD | perl -ne 'chomp and print'`
+
+# main distro
+SRCDIR=deadbeef-$VERSION
+PLUGDIR=$SRCDIR/plugins
+DOCDIR=$SRCDIR/doc
+PIXMAPDIR=$SRCDIR/pixmaps
+
+rm portable_out/build/deadbeef-$VERSION-static.tar.bz2
+
+cd portable
+tar jcvf ../portable_out/build/deadbeef-$VERSION-static.tar.bz2\
+    $SRCDIR/deadbeef\
+    $SRCDIR/deadbeef.png\
+    $DOCDIR\
+    $PLUGDIR/aac.so\
+    $PLUGDIR/adplug.so\
+    $PLUGDIR/alsa.so\
+    $PLUGDIR/artwork.so\
+    $PLUGDIR/cdda.so\
+    $PLUGDIR/dca.so\
+    $PLUGDIR/ddb_gui_GTK2.fallback.so\
+    $PLUGDIR/ddb_gui_GTK2.so\
+    $PLUGDIR/ffap.so\
+    $PLUGDIR/ffmpeg.so\
+    $PLUGDIR/flac.so\
+    $PLUGDIR/gme.so\
+    $PLUGDIR/hotkeys.so\
+    $PLUGDIR/lastfm.so\
+    $PLUGDIR/m3u.so\
+    $PLUGDIR/mms.so\
+    $PLUGDIR/mpgmad.so\
+    $PLUGDIR/musepack.so\
+    $PLUGDIR/notify.so\
+    $PLUGDIR/nullout.so\
+    $PLUGDIR/oss.so\
+    $PLUGDIR/shellexec.so\
+    $PLUGDIR/sid.so\
+    $PLUGDIR/sndfile.so\
+    $PLUGDIR/supereq.so\
+    $PLUGDIR/tta.so\
+    $PLUGDIR/vfs_curl.so\
+    $PLUGDIR/vfs_zip.so\
+    $PLUGDIR/vorbis.so\
+    $PLUGDIR/vtx.so\
+    $PLUGDIR/wavpack.so\
+    $PLUGDIR/wildmidi.so\
+    $PIXMAPDIR
+cd ..
