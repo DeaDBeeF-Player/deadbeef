85fd12a315d55de041ac3b5ef1ef22c27715bd65
waker <wakeroid@gmail.com>
2011-04-30 22:15:04 +0200

    fixed ao static build

--- scripts/portable_build.sh
+++ scripts/portable_build.sh
@@ -3,22 +3,24 @@ VERSION=`cat PORTABLE_VERSION | perl -ne 'chomp and print'`
 ORIGIN=`pwd | perl -ne 'chomp and print'`
 export APBUILD_STATIC_LIBGCC=1
 
-#./autogen.sh
+./autogen.sh
 CC=$ORIGIN/tools/apbuild/apgcc
 CXX=$ORIGIN/tools/apbuild/apgcc
 
 ./configure --enable-staticlink --enable-portable --disable-nls --disable-artwork-imlib2
 sed -i 's/-lstdc++ -lm -lgcc_s -lc -lgcc_s/-lm -lc/g' libtool
-#make clean
-#make -j9
+make clean
+make -j9
 
-ZLIB_LIBS=$ORIGIN/lib-x86-32/libz.a
-CFLAGS=-I $ORIGIN/lib-x86-32/include
+#ZLIB_LIBS="$ORIGIN/lib-x86-32/libz.a"
+CFLAGS="-I$ORIGIN/lib-x86-32/include"
 
-for i in dumb shn ao ; do
+for i in shn dumb ao ; do
+    echo cd
     cd $ORIGIN/plugins/$i
     make clean
-    make -j8 ZLIB_LIBS=$ZLIB_LIBS STATIC_CFLAGS=$CFLAGS CC=$CC CXX=$CXX
+    echo making $ORIGIN/plugins/$i
+    make -j8 STATIC_CFLAGS="$CFLAGS" CC=$CC CXX=$CXX
 done
 
 cd $ORIGIN
