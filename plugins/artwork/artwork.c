1f856c831e5be0ae45cbd67d75a6d411d3684f08
waker <wakeroid@gmail.com>
2011-05-13 20:45:01 +0200

    artwork: don't display jpg error messages when scaling covers

--- plugins/artwork/artwork.c
+++ plugins/artwork/artwork.c
@@ -196,9 +196,7 @@ my_error_exit (j_common_ptr cinfo)
   /* cinfo->err really points to a my_error_mgr struct, so coerce pointer */
   my_error_ptr myerr = (my_error_ptr) cinfo->err;
 
-  /* Always display the message. */
-  /* We could postpone this until after returning, if we chose. */
-  (*cinfo->err->output_message) (cinfo);
+//  (*cinfo->err->output_message) (cinfo);
 
   /* Return control to the setjmp point */
   longjmp(myerr->setjmp_buffer, 1);
