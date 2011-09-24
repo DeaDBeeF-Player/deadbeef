4953fd594353cde2030ce8ad20d9093524fa305a
waker <wakeroid@gmail.com>
2011-09-24 13:01:30 +0200

    always accept new content-length on request without checks

--- plugins/vfs_curl/vfs_curl.c
+++ plugins/vfs_curl/vfs_curl.c
@@ -475,12 +475,9 @@ http_content_header_handler (void *ptr, size_t size, size_t nmemb, void *stream)
                 free (fp->content_type);
             }
             fp->content_type = strdup (value);
-            fp->length = -1;
         }
         else if (!strcasecmp (key, "Content-Length")) {
-            if (fp->length < 0) {
-                fp->length = atoi (value);
-            }
+            fp->length = atoi (value);
         }
         else if (!strcasecmp (key, "icy-name")) {
             if (fp->track) {
