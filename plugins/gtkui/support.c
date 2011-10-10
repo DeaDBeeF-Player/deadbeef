790d628431cc09e784ef43195e14c6109a865b98
waker <wakeroid@gmail.com>
2011-10-10 20:36:07 +0200

    few fixes for compatibility with gtk<2.22

--- plugins/gtkui/support.c
+++ plugins/gtkui/support.c
@@ -147,7 +147,48 @@ GtkWidget *
 gtk_combo_box_entry_new_text(void) {
     GtkWidget *w = gtk_combo_box_text_new_with_entry ();
 }
+
 void
 gtk_dialog_set_has_separator (GtkDialog *dlg, gboolean has) {
 }
+#endif
+
+#if !GTK_CHECK_VERSION(2,22,0)
+GdkDragAction
+gdk_drag_context_get_selected_action (GdkDragContext *context) {
+    return context->action;
+}
+#endif
+
+#if !GTK_CHECK_VERSION(2,24,0)
+#define GTK_COMBO_BOX_TEXT GTK_COMBO_BOX
+GtkWidget *
+gtk_combo_box_text_new () {
+    return gtk_combo_box_new_text ();
+}
+
+GtkWidget *
+gtk_combo_box_text_new_with_entry   (void) {
+    return gtk_combo_box_new_with_entry ();
+}
+
+void
+gtk_combo_box_text_append_text (GtkComboBoxText *combo_box, const gchar *text) {
+    gtk_combo_box_append_text (combo_box, text);
+}
+
+void
+gtk_combo_box_text_insert_text (GtkComboBoxText *combo_box, gint position, const gchar *text) {
+    gtk_combo_box_insert_text (combo_box, position, text);
+}
+
+void
+gtk_combo_box_text_prepend_text (GtkComboBoxText *combo_box, const gchar *text) {
+    gtk_combo_box_prepend_text (combo_box, text);
+}
+gchar *
+gtk_combo_box_text_get_active_text  (GtkComboBoxText *combo_box) {
+    return gtk_combo_box_get_active_text (combo_box);
+}
+
 #endif
