2508973576b0855720787e67159dca615ed2549e
waker <wakeroid@gmail.com>
2012-02-02 20:17:19 +0100

    restored gtk<2.24 compatibility in converter gui plugin

--- plugins/converter/support.h
+++ plugins/converter/support.h
@@ -75,3 +75,14 @@ gtk_combo_box_entry_new_text(void);
 void
 gtk_dialog_set_has_separator (GtkDialog *dlg, gboolean has);
 #endif
+
+#if !GTK_CHECK_VERSION(2,24,0)
+#define GTK_COMBO_BOX_TEXT GTK_COMBO_BOX
+typedef GtkComboBox GtkComboBoxText;
+GtkWidget *gtk_combo_box_text_new ();
+GtkWidget *gtk_combo_box_text_new_with_entry   (void);
+void gtk_combo_box_text_append_text (GtkComboBoxText *combo_box, const gchar *text);
+void gtk_combo_box_text_insert_text (GtkComboBoxText *combo_box, gint position, const gchar *text);
+void gtk_combo_box_text_prepend_text (GtkComboBoxText *combo_box, const gchar *text);
+gchar *gtk_combo_box_text_get_active_text  (GtkComboBoxText *combo_box);
+#endif
