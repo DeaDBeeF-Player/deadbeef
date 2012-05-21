b5e4e556610b348db74edf5bea3ccec0caaafd07
waker <wakeroid@gmail.com>
2012-05-21 22:16:27 +0200

    bsd sys/syslimits.h fix

--- plugins/cdda/cdda.c
+++ plugins/cdda/cdda.c
@@ -18,11 +18,17 @@
 
 /* screwed/maintained by Alexey Yakovenko <waker@users.sourceforge.net> */
 
+#ifdef HAVE_CONFIG_H
+#  include "../../config.h"
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 #include <sys/types.h>
+#if HAVE_SYS_SYSLIMITS_H
+#include <sys/syslimits.h>
+#endif
 
 #include <cdio/cdio.h>
 #include <cdio/cdtext.h>
