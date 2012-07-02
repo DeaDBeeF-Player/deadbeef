9feb9ac65e649d2df62a112996bb645fd87fc4d1
Alex-P. Natsios <drakevr@linuxteam.cs.teilar.gr>
2012-07-02 23:51:51 +0300

    Prettified and demystified autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-aclocal
-autoheader
-libtoolize
-autoconf
-automake -a -c
+echo "Running autopoint..."; autopoint -f || :
+echo "Running aclocal..."; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
+echo "Running autoheader..."; autoheader || exit 1
+echo "Running autoconf..."; autoconf || exit 1
+echo "Running libtoolize..."; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
+echo "Running automake..."; automake --add-missing --copy --gnu || exit 1
+echo "Running intltoolize"; intltoolize --force
