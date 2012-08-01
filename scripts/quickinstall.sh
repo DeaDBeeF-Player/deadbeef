44d23a9c9d474a56c34265d3879e9082ae25b794
waker <wakeroid@gmail.com>
2012-08-01 22:27:36 +0200

    added new ALAC plugin

--- scripts/quickinstall.sh
+++ scripts/quickinstall.sh
@@ -46,3 +46,4 @@ cp ./plugins/converter/.libs/converter_gtk3.so /usr/local/lib/deadbeef/
 cp ./plugins/soundtouch/ddb_soundtouch.so /usr/local/lib/deadbeef/
 cp ./plugins/vfs_zip/.libs/vfs_zip.so /usr/local/lib/deadbeef/
 cp ./plugins/mono2stereo/.libs/ddb_mono2stereo.so /usr/local/lib/deadbeef/
+cp ./plugins/alac/.libs/alac.so /usr/local/lib/deadbeef/
