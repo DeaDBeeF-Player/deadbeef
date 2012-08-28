01a6195dcc9ef5a873ed821d20d29378ea83634a
waker <wakeroid@gmail.com>
2012-08-28 22:38:22 +0200

    aac: added chunk and sample index creation

--- plugins/libmp4ff/mp4ffint.h
+++ plugins/libmp4ff/mp4ffint.h
@@ -189,6 +189,19 @@ typedef struct
     int32_t *stts_sample_count;
     int32_t *stts_sample_delta;
 
+    /* chunk index generated in mp4ff_create_chunks_index */
+    // i_chunk_count = stsc_entry_count
+    // chunk->i_sample_count = stsc_samples_per_chunk[i_chunk]
+    // chunk->i_sample_description_index = stsc_sample_desc_index[chunk]
+    // trk->p_sample_size[i_sample] = stsz_sample_size ? stsz_sample_size : stsz_table[i_sample]
+    int32_t *chunk_sample_first;
+    int32_t *chunk_first_dts;
+    int32_t *chunk_last_dts;
+    int32_t **p_sample_count_dts;
+    int32_t **p_sample_delta_dts;
+    int32_t **p_sample_count_pts;
+    int32_t **p_sample_offset_pts;
+
     /* stsc */
     int32_t stsc_entry_count;
     int32_t *stsc_first_chunk;
@@ -211,7 +224,7 @@ typedef struct
     uint32_t maxBitrate;
     uint32_t avgBitrate;
 
-    uint32_t timeScale;
+    uint32_t timeScale; // FIXME: check calculation
     uint64_t duration;
 
 } mp4ff_track_t;
@@ -343,6 +356,8 @@ mp4ff_t *mp4ff_open_edit(mp4ff_callback_t *f);
 #endif
 void mp4ff_close(mp4ff_t *ff);
 //void mp4ff_track_add(mp4ff_t *f);
+int mp4ff_track_create_chunks_index(mp4ff_t *f, mp4ff_track_t *trk);
+int mp4ff_track_create_samples_index (mp4ff_t *f, mp4ff_track_t *trk);
 int32_t parse_sub_atoms(mp4ff_t *f, const uint64_t total_size,int meta_only);
 int32_t parse_atoms(mp4ff_t *f,int meta_only);
 
