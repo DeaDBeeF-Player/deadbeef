f684626f02f084afd082ede1955d911208b1fa2b
waker <wakeroid@gmail.com>
2012-10-14 21:41:19 +0200

    staticbuild: tell libtool to not hardcode library paths

--- scripts/static_build.sh
+++ scripts/static_build.sh
@@ -13,6 +13,7 @@ cd ../../
 export APBUILD_STATIC_LIBGCC=1
 CC=$AP/apgcc CXX=$AP/apgcc ./configure --enable-staticlink --disable-artwork-imlib2 --enable-ffmpeg --prefix=/opt/deadbeef
 sed -i 's/-lstdc++ -lm -lgcc_s -lc -lgcc_s/-lm -lc/g' libtool
+sed -i 's/hardcode_into_libs=yes/hardcode_into_libs=no/g' libtool
 make clean
 make DESTDIR=`pwd`/static/$ARCH/deadbeef-$VERSION -j8 install
 
