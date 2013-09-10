8aaa885aae14ef3bd350b847e4e410d5dc3d0197
Alexey Yakovenko <waker@users.sourceforge.net>
2013-09-10 20:35:45 +0200

    fixed ffap build on osx

--- yasmwrapper.sh
+++ yasmwrapper.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 ARGS=""
 for i in $@ ; do
     #if [ "$i" != "-fPIC" ] && [ "$i" != "-DPIC" ]; then
-    if [ "$i" != "-fPIC" ]; then
+    if [ "$i" != "-fPIC" -a "$i" != "-fno-common" ]; then
         ARGS="$ARGS $i"
     fi
 done
