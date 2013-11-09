e2a3d27f79358847449eace52512a4e70a020e32
Alexey Yakovenko <waker@users.sourceforge.net>
2013-11-09 12:46:25 +0100

    gtkui: fixed cover widget not updating when transitioning to next track

--- plugins/gtkui/widgets.c
+++ plugins/gtkui/widgets.c
@@ -2194,6 +2194,9 @@ static int
 coverart_message (ddb_gtkui_widget_t *w, uint32_t id, uintptr_t ctx, uint32_t p1, uint32_t p2) {
     w_coverart_t *ca = (w_coverart_t *)w;
     switch (id) {
+    case DB_EV_SONGSTARTED:
+        g_idle_add (coverart_redraw_cb, w);
+        break;
     case DB_EV_TRACKINFOCHANGED:
         {
             ddb_event_track_t *ev = (ddb_event_track_t *)ctx;
