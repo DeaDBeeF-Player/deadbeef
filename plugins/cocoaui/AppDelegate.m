7e9d75953a936510f0882b969032a9816647055f
Alexey Yakovenko <waker@users.sourceforge.net>
2013-11-17 13:32:04 +0100

    added xcode project files

---
+++ plugins/cocoaui/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  deadbeef
+//
+//  Created by Oleksiy Yakovenko on 16/11/13.
+//  Copyright (c) 2013 Alexey Yakovenko. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
