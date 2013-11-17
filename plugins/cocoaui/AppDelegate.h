2014c57703afbff41138c79a08ac170991cd3b06
Alexey Yakovenko <waker@users.sourceforge.net>
2013-11-17 13:32:04 +0100

    added xcode project files

---
+++ plugins/cocoaui/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  deadbeef
+//
+//  Created by Oleksiy Yakovenko on 16/11/13.
+//  Copyright (c) 2013 Alexey Yakovenko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ plugins/cocoaui/main.m
@@ -0,0 +1,33 @@
+//
+//  main.m
+//  deadbeef
+//
+//  Created by Oleksiy Yakovenko on 16/11/13.
+//  Copyright (c) 2013 Alexey Yakovenko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#include "deadbeef.h"
+
+int cocoaui_start(void)
+{
+	char *argv[1];
+	argv[0] = "FIXME";
+	return NSApplicationMain(1, argv);
+}
+
+DB_gui_t plugin = {
+	.plugin.type = DB_PLUGIN_GUI,
+	.plugin.api_vmajor = 1,
+	.plugin.api_vminor = 5,
+	.plugin.version_major = 1,
+	.plugin.version_minor = 0,
+	.plugin.id = "cocoaui",
+	.plugin.name = "Cocoa UI",
+	.plugin.start = cocoaui_start
+	// NSApplicationMain doesn't return, so it doesn't seem it's possible to cleanup
+};
+
+DB_plugin_t *cocoaui_load (void) {
+	return (DB_plugin_t *)&plugin;
+}
