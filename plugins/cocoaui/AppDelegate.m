80eb06d2b4b419fbb0aced5887a6576fd2bb8a24
Alexey Yakovenko <waker@users.sourceforge.net>
2013-11-17 18:42:05 +0100

    osx: added buttons with handlers

--- plugins/cocoaui/AppDelegate.m
+++ plugins/cocoaui/AppDelegate.m
@@ -1,18 +1,28 @@
-//
-//  AppDelegate.m
-//  deadbeef
-//
-//  Created by Oleksiy Yakovenko on 16/11/13.
-//  Copyright (c) 2013 Alexey Yakovenko. All rights reserved.
-//
-
 #import "AppDelegate.h"
+#include "../../deadbeef.h"
+
+extern DB_functions_t *deadbeef;
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
 	// Insert code here to initialize your application
+}
+- (IBAction)playClicked:(id)sender {
+    deadbeef->sendmessage(DB_EV_PLAY_CURRENT, 0, 0, 0);
+}
+- (IBAction)pauseClicked:(id)sender {
+    deadbeef->sendmessage(DB_EV_TOGGLE_PAUSE, 0, 0, 0);
+}
+- (IBAction)prevClicked:(id)sender {
+    deadbeef->sendmessage(DB_EV_PREV, 0, 0, 0);
+}
+- (IBAction)nextClicked:(id)sender {
+    deadbeef->sendmessage(DB_EV_NEXT, 0, 0, 0);
+}
+- (IBAction)stopClicked:(id)sender {
+    deadbeef->sendmessage(DB_EV_STOP, 0, 0, 0);
 }
 
 @end
