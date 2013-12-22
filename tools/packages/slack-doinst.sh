bf6552df8e93bbcbaf042fc45bee70f8f3b7d770
Alexey Yakovenko <waker@users.sourceforge.net>
2013-12-22 14:18:50 +0100

    added slackware package builder (untested)

---
+++ tools/packages/slack-doinst.sh
@@ -0,0 +1,9 @@
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
