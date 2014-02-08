32a11747dd6849867b0bb934d6a7e89b496ee230
Alexey Yakovenko <waker@users.sourceforge.net>
2014-02-08 23:12:15 +0100

    take static build dependencies from static-deps folder, instead of ../deadbeef-deps

--- scripts/static_build.sh
+++ scripts/static_build.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 VERSION=`cat PORTABLE_VERSION | perl -ne 'chomp and print'`
 ORIGIN=`pwd | perl -ne 'chomp and print'`
+STATIC_DEPS=static-deps
 AP=$ORIGIN/tools/apbuild
 #ARCH=`uname -m | perl -ne 'chomp and print'`
 if [[ "$ARCH" == "i686" ]]; then
-    export CFLAGS="-m32 -I$ORIGIN/../deadbeef-deps/lib-x86-32/include/i386-linux-gnu"
+    export CFLAGS="-m32 -I$ORIGIN/$STATIC_DEPS/lib-x86-32/include/i386-linux-gnu"
     export CXXFLAGS=$CFLAGS
-    export LDFLAGS="-m32 -L$ORIGIN/../deadbeef-deps/lib-x86-32/lib -L$ORIGIN/../deadbeef-deps/lib-x86-32/lib/i386-linux-gnu"
+    export LDFLAGS="-m32 -L$ORIGIN/$STATIC_DEPS/lib-x86-32/lib -L$ORIGIN/$STATIC_DEPS/lib-x86-32/lib/i386-linux-gnu"
     export CONFIGURE_FLAGS="--build=i686-unknown-linux-gnu"
-    export LD_LIBRARY_PATH="$ORIGIN/../deadbeef-deps/lib-x86-32/lib"
-    export PKG_CONFIG_PATH="$ORIGIN/../deadbeef-deps/lib-x86-32/lib/pkgconfig"
+    export LD_LIBRARY_PATH="$ORIGIN/$STATIC_DEPS/lib-x86-32/lib"
+    export PKG_CONFIG_PATH="$ORIGIN/$STATIC_DEPS/lib-x86-32/lib/pkgconfig"
 elif [[ "$ARCH" == "x86_64" ]]; then
-    export CFLAGS='-m64 -I$ORIGIN/../deadbeef-deps/lib-x86-64/include/x86_64-linux-gnu'
-    export LDFLAGS="-m64 -L$ORIGIN/../deadbeef-deps/lib-x86-64/lib -L$ORIGIN/../deadbeef-deps/lib-x86-64/lib/x86_64-linux-gnu"
+    export CFLAGS="-m64 -I$ORIGIN/$STATIC_DEPS/lib-x86-64/include/x86_64-linux-gnu"
+    export LDFLAGS="-m64 -L$ORIGIN/$STATIC_DEPS/lib-x86-64/lib -L$ORIGIN/$STATIC_DEPS/lib-x86-64/lib/x86_64-linux-gnu"
     export CONFIGURE_FLAGS="--build=x86_64-unknown-linux-gnu"
-    export LD_LIBRARY_PATH="$ORIGIN/../deadbeef-deps/lib-x86-64/lib"
-    export PKG_CONFIG_PATH="$ORIGIN/../deadbeef-deps/lib-x86-64/lib/pkgconfig"
+    export LD_LIBRARY_PATH="$ORIGIN/$STATIC_DEPS/lib-x86-64/lib"
+    export PKG_CONFIG_PATH="$ORIGIN/$STATIC_DEPS/lib-x86-64/lib/pkgconfig"
 else
     echo unknown arch $ARCH
     exit -1
