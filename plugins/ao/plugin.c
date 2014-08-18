ed7b1d6b6e8f683b2bf232364a340a7eaeb25241
Alexey Yakovenko <waker@users.sourceforge.net>
2014-08-18 22:53:51 +0200

    ao: better error reporting on failed files

--- plugins/ao/plugin.c
+++ plugins/ao/plugin.c
@@ -214,7 +214,7 @@ static DB_playItem_t *
 aoplug_insert (ddb_playlist_t *plt, DB_playItem_t *after, const char *fname) {
     DB_FILE *fp = deadbeef->fopen (fname);
     if (!fp) {
-        trace ("psf: failed to fopen %s\n", fname);
+        fprintf (stderr, "psf: failed to fopen %s\n", fname);
         return NULL;
     }
 
@@ -237,6 +237,7 @@ aoplug_insert (ddb_playlist_t *plt, DB_playItem_t *after, const char *fname) {
 
     int type = ao_identify (buffer);
     if (type < 0) {
+        fprintf (stderr, "aosdk can't identify the contents of the file %s\n", fname);
         free (buffer);
         return NULL;
     }
