eb716a1c50c004d1cea4790b6f3acae2bcef1530
Alexey Yakovenko <wakeroid@gmail.com>
2014-08-27 22:46:10 +0200

    cocoa: new seekbar prototype

---
+++ plugins/cocoaui/DdbSeekBar.h
@@ -0,0 +1,15 @@
+//
+//  DdbSeekBar.h
+//  deadbeef
+//
+//  Created by waker on 27/08/14.
+//  Copyright (c) 2014 Alexey Yakovenko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DdbSeekBar : NSSliderCell
+
+@property NSImage *knobImg;
+
+@end
