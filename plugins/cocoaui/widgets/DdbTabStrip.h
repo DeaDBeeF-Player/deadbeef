719a1b94e65e526460b77f923e31a1923f5c34cb
Alexey Yakovenko <wakeroid@gmail.com>
2014-09-03 22:21:12 +0200

    cocoa: tabstrip WIP

---
+++ plugins/cocoaui/widgets/DdbTabStrip.h
@@ -0,0 +1,16 @@
+//
+//  DdbTabStrip.h
+//  deadbeef
+//
+//  Created by waker on 03/09/14.
+//  Copyright (c) 2014 Alexey Yakovenko. All rights reserved.
+//
+
+#import "DdbWidget.h"
+
+@interface DdbTabStrip : DdbWidget
+@property int hscrollpos;
+@property int dragging;
+@property int prepare;
+@property int movepos;
+@end
