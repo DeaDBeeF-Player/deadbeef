aa4e540facbc5a927d921f14c8ecd31b3bba19ab
Alexey Yakovenko <waker@users.sourceforge.net>
2014-09-22 15:57:39 +0200

    cocoa: added DdbListviewWidget with proper event handling

---
+++ plugins/cocoaui/widgets/DdbPlaylistWidget.h
@@ -0,0 +1,13 @@
+//
+//  DdbPlaylistWidgetView.h
+//  deadbeef
+//
+//  Created by Oleksiy Yakovenko on 22/09/14.
+//  Copyright (c) 2014 Alexey Yakovenko. All rights reserved.
+//
+
+#import "DdbWidget.h"
+
+@interface DdbPlaylistWidgetView : DdbWidget
+
+@end
---
+++ plugins/cocoaui/widgets/DdbPlaylistWidget.m
@@ -0,0 +1,29 @@
+//
+//  DdbPlaylistWidgetView.m
+//  deadbeef
+//
+//  Created by Oleksiy Yakovenko on 22/09/14.
+//  Copyright (c) 2014 Alexey Yakovenko. All rights reserved.
+//
+
+#import "DdbPlaylistWidgetView.h"
+
+@implementation DdbPlaylistWidgetView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
