534553fddeb8dfc297da32f32fb4d99ded5831ab
Alexey Yakovenko <wakeroid@gmail.com>
2014-10-06 21:52:43 +0200

    cocoa: get rid of bogus properties in ddbseekbar

--- plugins/cocoaui/DdbSeekBar.m
+++ plugins/cocoaui/DdbSeekBar.m
@@ -18,30 +18,23 @@
 
 @implementation DdbSeekBarCell
 
-@synthesize backCapLeft;
-@synthesize backCapRight;
-@synthesize backFiller;
-@synthesize frontCapLeft;
-@synthesize frontCapRight;
-@synthesize frontFiller;
-
 - (id)initWithCoder:(NSCoder *)decoder
 {
     self = [super initWithCoder:decoder];
     if (self) {
-        backCapLeft = [NSImage imageNamed:@"sb_back_cap_left.tiff"];
-        backCapRight = [NSImage imageNamed:@"sb_back_cap_right.tiff"];
-        backFiller = [NSImage imageNamed:@"sb_back_filler.tiff"];
-        frontCapLeft = [NSImage imageNamed:@"sb_front_cap_left.tiff"];
-        frontCapRight = [NSImage imageNamed:@"sb_front_cap_right.tiff"];
-        frontFiller = [NSImage imageNamed:@"sb_front_filler.tiff"];
+        _backCapLeft = [NSImage imageNamed:@"sb_back_cap_left.tiff"];
+        _backCapRight = [NSImage imageNamed:@"sb_back_cap_right.tiff"];
+        _backFiller = [NSImage imageNamed:@"sb_back_filler.tiff"];
+        _frontCapLeft = [NSImage imageNamed:@"sb_front_cap_left.tiff"];
+        _frontCapRight = [NSImage imageNamed:@"sb_front_cap_right.tiff"];
+        _frontFiller = [NSImage imageNamed:@"sb_front_filler.tiff"];
 
-        [backCapLeft setFlipped:YES];
-        [backCapRight setFlipped:YES];
-        [backFiller setFlipped:YES];
-        [frontCapLeft setFlipped:YES];
-        [frontCapRight setFlipped:YES];
-        [frontFiller setFlipped:YES];
+        [_backCapLeft setFlipped:YES];
+        [_backCapRight setFlipped:YES];
+        [_backFiller setFlipped:YES];
+        [_frontCapLeft setFlipped:YES];
+        [_frontCapRight setFlipped:YES];
+        [_frontFiller setFlipped:YES];
     }
     return self;
 }
@@ -57,23 +50,23 @@
     NSRect rc = controlView.bounds;
     [controlView setNeedsDisplayInRect:rc];
     
-    int h = [backFiller size].height;
+    int h = [_backFiller size].height;
     int y = rc.origin.y + (int)rc.size.height/2 - (int)h/2;
     rc.origin.y = y;
     rc.size.height = h;
     
-    NSSize backCapLeftSize = [backCapLeft size];
-    NSSize backCapRightSize = [backCapRight size];
-    NSSize frontCapLeftSize = [frontCapLeft size];
-    NSSize frontCapRightSize = [frontCapRight size];
+    NSSize backCapLeftSize = [_backCapLeft size];
+    NSSize backCapRightSize = [_backCapRight size];
+    NSSize frontCapLeftSize = [_frontCapLeft size];
+    NSSize frontCapRightSize = [_frontCapRight size];
     
     if (![self isEnabled]) {
-        [backCapLeft drawAtPoint:rc.origin fromRect:NSMakeRect(0, 0, backCapLeftSize.width, backCapLeftSize.height) operation:NSCompositeSourceOver fraction:1];
+        [_backCapLeft drawAtPoint:rc.origin fromRect:NSMakeRect(0, 0, backCapLeftSize.width, backCapLeftSize.height) operation:NSCompositeSourceOver fraction:1];
     }
     else {
-        [frontCapLeft drawAtPoint:rc.origin fromRect:NSMakeRect(0, 0, frontCapLeftSize.width, frontCapLeftSize.height) operation:NSCompositeSourceOver fraction:1];
+        [_frontCapLeft drawAtPoint:rc.origin fromRect:NSMakeRect(0, 0, frontCapLeftSize.width, frontCapLeftSize.height) operation:NSCompositeSourceOver fraction:1];
     }
-    [backCapRight drawAtPoint:NSMakePoint(rc.origin.x+rc.size.width-backCapRightSize.width, rc.origin.y) fromRect:NSMakeRect(0, 0, backCapLeftSize.width, backCapLeftSize.height) operation:NSCompositeSourceOver fraction:1];
+    [_backCapRight drawAtPoint:NSMakePoint(rc.origin.x+rc.size.width-backCapRightSize.width, rc.origin.y) fromRect:NSMakeRect(0, 0, backCapLeftSize.width, backCapLeftSize.height) operation:NSCompositeSourceOver fraction:1];
 
     rc.origin.x += backCapLeftSize.width;
     rc.size.width -= backCapLeftSize.width + backCapRightSize.width;
@@ -82,15 +75,15 @@
     [gc saveGraphicsState];
     NSPoint convPt = [controlView convertPoint:NSMakePoint(0,y) fromView:nil];
     [gc setPatternPhase:convPt];
-    [[NSColor colorWithPatternImage:backFiller] set];
+    [[NSColor colorWithPatternImage:_backFiller] set];
     [NSBezierPath fillRect:rc];
-    [[NSColor colorWithPatternImage:frontFiller] set];
+    [[NSColor colorWithPatternImage:_frontFiller] set];
     rc.size.width = (int)(rc.size.width * _value / ([self maxValue] - [self minValue]));
     [NSBezierPath fillRect:rc];
     [gc restoreGraphicsState];
     
     if ([self isEnabled]) {
-        [frontCapRight drawAtPoint:NSMakePoint(rc.origin.x+rc.size.width, rc.origin.y) fromRect:NSMakeRect(0, 0, frontCapRightSize.width, frontCapRightSize.height) operation:NSCompositeSourceOver fraction:1];
+        [_frontCapRight drawAtPoint:NSMakePoint(rc.origin.x+rc.size.width, rc.origin.y) fromRect:NSMakeRect(0, 0, frontCapRightSize.width, frontCapRightSize.height) operation:NSCompositeSourceOver fraction:1];
     }
 }
 
