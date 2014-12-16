1584144a131e5f42f9a6478fce4c95fb41944b3d
Alexey Yakovenko <wakeroid@gmail.com>
2014-12-16 20:34:02 +0100

    added mpg123 lib

---
+++ osx/mpg123/mpg123.h
@@ -0,0 +1,15 @@
+//
+//  mpg123.h
+//  deadbeef
+//
+//  Created by waker on 16/12/14.
+//  Copyright (c) 2014 Alexey Yakovenko. All rights reserved.
+//
+
+#ifndef deadbeef_mpg123_h
+#define deadbeef_mpg123_h
+
+#define MPG123_NO_CONFIGURE
+#include "mpg123.h.in"
+
+#endif
