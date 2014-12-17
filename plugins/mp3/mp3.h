5cd1206901238179fbf0156cbd948b65574184b6
Alexey Yakovenko <wakeroid@gmail.com>
2014-12-17 20:14:24 +0100

    mp3: moved libmad code into separate file; added decoder library abstraction layer; fixed makefiles

---
+++ plugins/mp3/mp3.h
@@ -0,0 +1,132 @@
+/*
+ MPEG decoder plugin for DeaDBeeF Player
+ Copyright (C) 2009-2014 Alexey Yakovenko
+
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+
+ 3. This notice may not be removed or altered from any source distribution.
+ */
+#ifndef deadbeef_mp3_h
+#define deadbeef_mp3_h
+
+#include "../../deadbeef.h"
+
+#ifdef USE_LIBMAD
+#include <mad.h>
+#endif
+
+extern DB_functions_t *deadbeef;
+
+#define READBUFFER 0x2800 // 10k is enough for single frame
+
+// vbrmethod constants
+#define XING_CBR  1
+#define XING_ABR  2
+#define XING_VBR1 3
+#define XING_VBR2 4
+#define XING_VBR3 5
+#define XING_VBR4 6
+#define XING_CBR2 8
+#define XING_ABR2 9
+#define DETECTED_VBR 100
+
+// xing header flags
+#define FRAMES_FLAG     0x0001
+#define BYTES_FLAG      0x0002
+#define TOC_FLAG        0x0004
+#define VBR_SCALE_FLAG  0x0008
+
+struct mp3_decoder_api_s;
+
+typedef struct {
+    DB_FILE *file;
+    DB_playItem_t *it;
+
+    // input buffer, for MPEG data
+    char input[READBUFFER];
+    int remaining;
+
+    // output buffer, supplied by player
+    int readsize;
+    int decode_remaining; // number of decoded samples of current mpeg frame
+    char *out;
+
+    // information, filled by cmp3_scan_stream
+    int version;
+    int layer;
+    int bitrate;
+    int samplerate;
+    int packetlength;
+    int bitspersample;
+    int channels;
+    float duration;
+
+    // currentsample and totalsamples are in the entire file scope (delay/padding inclusive)
+    int currentsample;
+    int totalsamples;
+
+    int skipsamples;
+
+    int64_t startoffset; // in bytes (id3v2, xing/lame)
+    int64_t endoffset; // in bytes (apev2, id3v1)
+
+    // startsample and endsample exclude delay/padding
+    int startsample;
+    int endsample;
+
+    // number of samples to skip at the start/end of file
+    int delay;
+    int padding;
+
+    float avg_packetlength;
+    int avg_samplerate;
+    int avg_samples_per_frame;
+    int nframes;
+    int last_comment_update;
+    int vbr;
+    int have_xing_header;
+    int lead_in_frames;
+} buffer_t;
+
+typedef struct {
+    DB_fileinfo_t info;
+    buffer_t buffer;
+    union {
+#ifdef USE_LIBMAD
+        struct {
+            struct mad_stream mad_stream;
+            struct mad_frame mad_frame;
+            struct mad_synth mad_synth;
+        };
+#endif
+#ifdef USE_LIBMPG123
+        struct {
+        };
+#endif
+    };
+
+    struct mp3_decoder_api_s *dec;
+} mp3_info_t;
+
+typedef struct mp3_decoder_api_s {
+    void (*init)(mp3_info_t *info);
+    void (*free)(mp3_info_t *info);
+    void (*decode)(mp3_info_t *info);
+    int (*stream_frame)(mp3_info_t *info);
+} mp3_decoder_api_t;
+
+#endif
---
+++ plugins/mp3/mp3_mad.c
@@ -0,0 +1,234 @@
+/*
+ MPEG decoder plugin for DeaDBeeF Player
+ Copyright (C) 2009-2014 Alexey Yakovenko
+
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+
+ 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#include "mp3_mad.h"
+
+//#define trace(...) { fprintf(stderr, __VA_ARGS__); }
+#define trace(fmt,...)
+
+void
+mp3_mad_init (mp3_info_t *info) {
+    mad_stream_init(&info->mad_stream);
+    mad_stream_options (&info->mad_stream, MAD_OPTION_IGNORECRC);
+    mad_frame_init(&info->mad_frame);
+    mad_synth_init(&info->mad_synth);
+}
+
+void
+mp3_mad_free (mp3_info_t *info) {
+    mad_synth_finish (&info->mad_synth);
+    mad_frame_finish (&info->mad_frame);
+    mad_stream_finish (&info->mad_stream);
+}
+
+/****************************************************************************
+ * Converts a sample from libmad's fixed point number format to a signed	*
+ * short (16 bits).															*
+ ****************************************************************************/
+static inline int16_t
+MadFixedToSshort(mad_fixed_t Fixed)
+{
+    /* A fixed point number is formed of the following bit pattern:
+     *
+     * SWWWFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+     * MSB                          LSB
+     * S ==> Sign (0 is positive, 1 is negative)
+     * W ==> Whole part bits
+     * F ==> Fractional part bits
+     *
+     * This pattern contains MAD_F_FRACBITS fractional bits, one
+     * should alway use this macro when working on the bits of a fixed
+     * point number. It is not guaranteed to be constant over the
+     * different platforms supported by libmad.
+     *
+     * The signed short value is formed, after clipping, by the least
+     * significant whole part bit, followed by the 15 most significant
+     * fractional part bits. Warning: this is a quick and dirty way to
+     * compute the 16-bit number, madplay includes much better
+     * algorithms.
+     */
+
+    /* Clipping */
+    if(Fixed>=MAD_F_ONE)
+        return(32767);
+    if(Fixed<=-MAD_F_ONE)
+        return(-32768);
+
+    /* Conversion. */
+    Fixed=Fixed>>(MAD_F_FRACBITS-15);
+    return((signed short)Fixed);
+}
+
+static inline float
+MadFixedToFloat (mad_fixed_t Fixed) {
+    return (float)((Fixed) / (float)(1L << MAD_F_FRACBITS));
+}
+
+#define MadErrorString(x) mad_stream_errorstr(x)
+
+void
+mp3_mad_decode (mp3_info_t *info) {
+    // copy synthesized samples into readbuffer
+    int idx = info->mad_synth.pcm.length-info->buffer.decode_remaining;
+    // stereo
+    if (MAD_NCHANNELS(&info->mad_frame.header) == 2 && info->info.fmt.channels == 2) {
+        while (info->buffer.decode_remaining > 0 && info->buffer.readsize > 0) {
+            *((int16_t*)info->buffer.out) = MadFixedToSshort (info->mad_synth.pcm.samples[0][idx]);
+            info->buffer.readsize -= 2;
+            info->buffer.out += 2;
+            *((int16_t*)info->buffer.out) = MadFixedToSshort (info->mad_synth.pcm.samples[1][idx]);
+            info->buffer.readsize -= 2;
+            info->buffer.out += 2;
+            info->buffer.decode_remaining--;
+            idx++;
+        }
+    }
+    // mono
+    else if (MAD_NCHANNELS(&info->mad_frame.header) == 1 && info->info.fmt.channels == 1){
+        while (info->buffer.decode_remaining > 0 && info->buffer.readsize > 0) {
+            *((int16_t*)info->buffer.out) = MadFixedToSshort (info->mad_synth.pcm.samples[0][idx]);
+            info->buffer.readsize -= 2;
+            info->buffer.out += 2;
+            info->buffer.decode_remaining--;
+            idx++;
+        }
+    }
+    // workaround for bad mp3s that have both mono and stereo frames
+    else if (MAD_NCHANNELS(&info->mad_frame.header) == 1 && info->info.fmt.channels == 2) {
+        while (info->buffer.decode_remaining > 0 && info->buffer.readsize > 0) {
+            int16_t sample = MadFixedToSshort (info->mad_synth.pcm.samples[0][idx]);
+            *((int16_t*)info->buffer.out) = sample;
+            info->buffer.readsize -= 2;
+            info->buffer.out += 2;
+            *((int16_t*)info->buffer.out) = sample;
+            info->buffer.readsize -= 2;
+            info->buffer.out += 2;
+            info->buffer.decode_remaining--;
+            idx++;
+        }
+    }
+    else if (MAD_NCHANNELS(&info->mad_frame.header) == 2 && info->info.fmt.channels == 1) {
+        while (info->buffer.decode_remaining > 0 && info->buffer.readsize > 0) {
+            int16_t sample = MadFixedToSshort (info->mad_synth.pcm.samples[0][idx]);
+            *((int16_t*)info->buffer.out) = sample;
+            info->buffer.readsize -= 2;
+            info->buffer.out += 2;
+            info->buffer.decode_remaining--;
+            idx++;
+        }
+    }
+}
+
+int
+mp3_mad_stream_frame (mp3_info_t *info) {
+    int eof = 0;
+    while (!eof && (info->mad_stream.buffer == NULL || info->buffer.decode_remaining <= 0)) {
+        // read more MPEG data if needed
+        if(info->mad_stream.buffer==NULL || info->mad_stream.error==MAD_ERROR_BUFLEN) {
+            // copy part of last frame to beginning
+            if (info->mad_stream.next_frame && info->mad_stream.bufend <= info->mad_stream.next_frame) {
+                eof = 1;
+                break;
+            }
+            if (info->mad_stream.next_frame != NULL) {
+                info->buffer.remaining = info->mad_stream.bufend - info->mad_stream.next_frame;
+                memmove (info->buffer.input, info->mad_stream.next_frame, info->buffer.remaining);
+            }
+            int size = READBUFFER - info->buffer.remaining;
+            int bytesread = 0;
+            uint8_t *bytes = info->buffer.input + info->buffer.remaining;
+            bytesread = deadbeef->fread (bytes, 1, size, info->buffer.file);
+            if (!bytesread) {
+                // add guard
+                eof = 1;
+                memset (bytes, 0, 8);
+                bytesread = 8;
+            }
+            if (bytesread < size) {
+                // end of file
+                size -= bytesread;
+                bytes += bytesread;
+            }
+            bytesread += info->buffer.remaining;
+            mad_stream_buffer(&info->mad_stream,info->buffer.input,bytesread);
+            if (info->mad_stream.buffer==NULL) {
+                // check sync bits
+                if (bytes[0] != 0xff || (bytes[1]&(3<<5)) != (3<<5)) {
+                    trace ("mp3: read didn't start at frame boundary!\ncmp3_scan_stream is broken\n");
+                }
+                else {
+                    trace ("mp3: streambuffer=NULL\n");
+                }
+            }
+        }
+        info->mad_stream.error=0;
+
+        // decode next frame
+        if(mad_frame_decode(&info->mad_frame, &info->mad_stream))
+        {
+            if(MAD_RECOVERABLE(info->mad_stream.error))
+            {
+                if(info->mad_stream.error!=MAD_ERROR_LOSTSYNC) {
+                    //                    printf ("mp3: recoverable frame level error (%s)\n", MadErrorString(&info->stream));
+                }
+                if (info->buffer.lead_in_frames > 0) {
+                    info->buffer.lead_in_frames--;
+                }
+                continue;
+            }
+            else {
+                if(info->mad_stream.error==MAD_ERROR_BUFLEN) {
+                    //                    printf ("mp3: recoverable frame level error (%s)\n", MadErrorString(&info->stream));
+                    continue;
+                }
+                else
+                {
+                    //                    printf ("mp3: unrecoverable frame level error (%s).\n", MadErrorString(&info->stream));
+                    return -1; // fatal error
+                }
+            }
+        }
+        mad_synth_frame(&info->mad_synth,&info->mad_frame);
+        if (info->buffer.lead_in_frames > 0) {
+            info->buffer.lead_in_frames--;
+            info->buffer.decode_remaining = 0;
+            continue;
+        }
+
+        info->info.fmt.samplerate = info->mad_frame.header.samplerate;
+
+        // synthesize single frame
+        info->buffer.decode_remaining = info->mad_synth.pcm.length;
+        deadbeef->streamer_set_bitrate (info->mad_frame.header.bitrate/1000);
+        break;
+    }
+    
+    return eof;
+}
+
+mp3_decoder_api_t mad_api = {
+    .init = mp3_mad_init,
+    .free = mp3_mad_free,
+    .decode = mp3_mad_decode,
+    .stream_frame = mp3_mad_stream_frame,
+};
