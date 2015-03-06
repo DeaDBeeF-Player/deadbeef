8b94b11308991cab7e9e499b2d15772291c2f04c
Alexey Yakovenko <wakeroid@gmail.com>
2015-03-06 22:05:07 +0100

    sc68: added linux makefile

--- scripts/quickinstall.sh
+++ scripts/quickinstall.sh
@@ -53,3 +53,4 @@ cp ./plugins/wma/.libs/wma.so $PREFIX/lib/deadbeef/
 cp ./plugins/pltbrowser/.libs/pltbrowser_gtk2.so $PREFIX/lib/deadbeef/
 cp ./plugins/pltbrowser/.libs/pltbrowser_gtk3.so $PREFIX/lib/deadbeef/
 cp ./plugins/coreaudio/.libs/coreaudio.so $PREFIX/lib/deadbeef/
+cp ./plugins/sc68/.libs/in_sc68.so $PREFIX/lib/deadbeef/
