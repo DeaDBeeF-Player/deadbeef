391e29c4659083d259e292a630c0428816435fcd
Alexey Yakovenko <wakeroid@gmail.com>
2015-06-07 13:10:00 +0200

    cocoaui: added TF help button to the custom sort dialog

---
+++ plugins/cocoaui/DdbTitleFormattingHelpButton.h
@@ -0,0 +1,13 @@
+//
+//  DdbTitleFormattingHelpButton.h
+//  deadbeef
+//
+//  Created by waker on 07/06/15.
+//  Copyright (c) 2015 Alexey Yakovenko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DdbTitleFormattingHelpButton : NSButton
+
+@end
