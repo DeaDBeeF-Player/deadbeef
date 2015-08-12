aa31d1029e4ea654d9b88c26f85a850005e60d61
Alexey Yakovenko <wakeroid@gmail.com>
2015-08-12 21:17:53 +0200

    gtkui: switched all our source code to ZLIB license, updated the licensing information

--- plugins/gtkui/ddbequalizer.c
+++ plugins/gtkui/ddbequalizer.c
@@ -1,22 +1,27 @@
 /*
-    SuperEQ GTK Widget for for DeaDBeeF
-    Copyright (C) 2010 Viktor Semykin <thesame.ml@gmail.com>
+    DeaDBeeF -- the music player
+    Copyright (C) 2009-2015 Alexey Yakovenko and other contributors
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 2 of the License, or
-    (at your option) any later version.
+    This software is provided 'as-is', without any express or implied
+    warranty.  In no event will the authors be held liable for any damages
+    arising from the use of this software.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    Permission is granted to anyone to use this software for any purpose,
+    including commercial applications, and to alter it and redistribute it
+    freely, subject to the following restrictions:
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+
+    2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+
+    3. This notice may not be removed or altered from any source distribution.
 */
-/* sripped down and polished by Alexey Yakovenko <waker@users.sourceforge.net>*/
 
+// Original Vala code: Copyright (C) 2010 Viktor Semykin <thesame.ml@gmail.com>
 #include <glib.h>
 #include <glib-object.h>
 #include <stdlib.h>
