76f6425dfef60ab3d37035c198d364a4cb9f8453
Alexey Yakovenko <waker@users.sourceforge.net>
2015-09-04 14:54:05 +0200

    gme: reimplemented the Bml_Parser

--- plugins/gme/game-music-emu-0.6pre/gme/Bml_Parser.cpp
+++ plugins/gme/game-music-emu-0.6pre/gme/Bml_Parser.cpp
@@ -4,321 +4,162 @@
 
 #include "Bml_Parser.h"
 
-const char * strchr_limited( const char * in, const char * end, char c )
-{
-    while ( in < end && *in != c ) ++in;
-    if ( in < end ) return in;
-    else return 0;
+Bml_Parser::Bml_Parser() {
+    nodes = NULL;
+    tail = NULL;
 }
 
-Bml_Node Bml_Node::emptyNode;
-
-Bml_Node::Bml_Node()
-{
-    name = 0;
-    value = 0;
+Bml_Parser::~Bml_Parser() {
+    clearDocument();
 }
 
-Bml_Node::Bml_Node(char const* name, size_t max_length)
+void Bml_Parser::clearDocument ()
 {
-    size_t length = 0;
-    char const* ptr = name;
-    while (*ptr && length < max_length) ++ptr, ++length;
-    this->name = new char[ length + 1 ];
-    memcpy( this->name, name, length );
-    this->name[ length ] = '\0';
-    value = 0;
-}
-
-Bml_Node::Bml_Node(const Bml_Node &in)
-{
-    size_t length;
-    name = 0;
-    if (in.name)
-    {
-        length = strlen(in.name);
-        name = new char[length + 1];
-        memcpy(name, in.name, length + 1);
+    while (nodes) {
+        if (nodes->key) {
+            free (nodes->key);
+        }
+        if (nodes->value) {
+            free (nodes->value);
+        }
+        nodes = nodes->next;
     }
-    value = 0;
-    if (in.value)
-    {
-        length = strlen(in.value);
-        value = new char[length + 1];
-        memcpy(value, in.value, length + 1);
+    tail = NULL;
+}
+
+void Bml_Parser::addNode (const char *path, const char *value)
+{
+    Bml_Node *node = new Bml_Node;
+    memset (node, 0, sizeof (Bml_Node));
+    node->key = strdup (path);
+    if (value) {
+        node->value = strdup (value);
     }
-    children = in.children;
-}
-
-Bml_Node::~Bml_Node()
-{
-    delete [] name;
-    delete [] value;
-}
-
-void Bml_Node::clear()
-{
-    delete [] name;
-    delete [] value;
-
-    name = 0;
-    value = 0;
-    children.resize( 0 );
-}
-
-void Bml_Node::setLine(const char *line, size_t max_length)
-{
-    delete [] name;
-    delete [] value;
-
-    name = 0;
-    value = 0;
-    
-    size_t length = 0;
-    const char * end = line;
-    while (*end && length < max_length) ++end;
-
-    const char * line_end = strchr_limited(line, end, '\n');
-    if ( !line_end ) line_end = end;
-
-    const char * first_letter = line;
-    while ( first_letter < line_end && *first_letter <= 0x20 ) first_letter++;
-
-    const char * colon = strchr_limited(first_letter, line_end, ':');
-    const char * last_letter = line_end - 1;
-
-    if (colon)
-    {
-        const char * first_value_letter = colon + 1;
-        while (first_value_letter < line_end && *first_value_letter <= 0x20) first_value_letter++;
-        last_letter = line_end - 1;
-        while (last_letter > first_value_letter && *last_letter <= 0x20) last_letter--;
-
-        value = new char[last_letter - first_value_letter + 2];
-        memcpy(value, first_value_letter, last_letter - first_value_letter + 1);
-        value[last_letter - first_value_letter + 1] = '\0';
-
-        last_letter = colon - 1;
+    if (tail) {
+        tail->next = node;
     }
-
-    while (last_letter > first_letter && *last_letter <= 0x20) last_letter--;
-
-    name = new char[last_letter - first_letter + 2];
-    memcpy(name, first_letter, last_letter - first_letter + 1);
-    name[last_letter - first_letter + 1] = '\0';
-}
-
-Bml_Node& Bml_Node::addChild(const Bml_Node &child)
-{
-    children.push_back(child);
-    return *(children.end() - 1);
-}
-
-const char * Bml_Node::getName() const
-{
-    return name;
-}
-
-const char * Bml_Node::getValue() const
-{
-    return value;
-}
-
-void Bml_Node::setValue(char const* value)
-{
-    delete [] this->value;
-    size_t length = strlen( value ) + 1;
-    this->value = new char[ length ];
-    memcpy( this->value, value, length );
-}
-
-size_t Bml_Node::getChildCount() const
-{
-    return children.size();
-}
-
-Bml_Node const& Bml_Node::getChild(size_t index) const
-{
-    return children[index];
-}
-
-Bml_Node & Bml_Node::walkToNode(const char *path, bool use_indexes)
-{
-    Bml_Node * next_node = NULL;
-    Bml_Node * node = this;
-    while ( *path )
-    {
-        bool item_found = false;
-        size_t array_index = 0;
-        const char * array_index_start = strchr( path, '[' );
-        const char * next_separator = strchr( path, ':' );
-        if ( !next_separator ) next_separator = path + strlen(path);
-        if ( use_indexes && array_index_start && array_index_start < next_separator )
-        {
-            char * temp;
-            array_index = strtoul( array_index_start + 1, &temp, 10 );
-        }
-        else
-        {
-            array_index_start = next_separator;
-        }
-        if ( use_indexes )
-        {
-            for ( std::vector<Bml_Node>::iterator it = node->children.begin(); it != node->children.end(); ++it )
-            {
-                if ( array_index_start - path == strlen(it->name) &&
-                    strncmp( it->name, path, array_index_start - path ) == 0 )
-                {
-                    next_node = &(*it);
-                    item_found = true;
-                    if ( array_index == 0 ) break;
-                    --array_index;
-                }
-                if (array_index)
-                    item_found = false;
-            }
-        }
-        else
-        {
-            for ( std::vector<Bml_Node>::iterator it = node->children.end(); it != node->children.begin(); )
-            {
-                --it;
-                if ( next_separator - path == strlen(it->name) &&
-                    strncmp( it->name, path, next_separator - path ) == 0 )
-                {
-                    next_node = &(*it);
-                    item_found = true;
-                    break;
-                }
-            }
-        }
-        if ( !item_found )
-        {
-            Bml_Node child( path, next_separator - path );
-            node = &(node->addChild( child ));
-        }
-        else
-            node = next_node;
-        if ( *next_separator )
-        {
-            path = next_separator + 1;
-        }
-        else break;
+    else {
+        nodes = node;
     }
-    return *node;
-}
-
-Bml_Node const& Bml_Node::walkToNode(const char *path) const
-{
-    Bml_Node const* next_node = NULL;
-    Bml_Node const* node = this;
-    while ( *path )
-    {
-        bool item_found = false;
-        size_t array_index = ~0;
-        const char * array_index_start = strchr( path, '[' );
-        const char * next_separator = strchr( path, ':' );
-        if ( !next_separator ) next_separator = path + strlen(path);
-        if ( array_index_start && array_index_start < next_separator )
-        {
-            char * temp;
-            array_index = strtoul( array_index_start + 1, &temp, 10 );
-        }
-        else
-        {
-            array_index_start = next_separator;
-        }
-        for ( std::vector<Bml_Node>::const_iterator it = node->children.begin(); it != node->children.end(); ++it )
-        {
-            if ( array_index_start - path == strlen(it->name) &&
-                 strncmp( it->name, path, array_index_start - path ) == 0 )
-            {
-                next_node = &(*it);
-                item_found = true;
-                if ( array_index == 0 ) break;
-                --array_index;
-            }
-        }
-        if ( !item_found ) return emptyNode;
-        node = next_node;
-        if ( *next_separator )
-        {
-            path = next_separator + 1;
-        }
-        else break;
-    }
-    return *node;
+    tail = node;
 }
 
 void Bml_Parser::parseDocument( const char * source, size_t max_length )
 {
-    size_t indents[100] = { 0 };
+    clearDocument();
+
+    const char *p = source;
+    const char *end = p + max_length;
+
+    char path[200] = "";
+
+    int indents[100];
     int num_indents = 0;
-    char last_name[1000] = "";
-    char current_path[1000] = "";
 
-    document.clear();
-
-    size_t last_indent = ~0;
-
-    Bml_Node node;
-    
-    size_t length = 0;
-    const char * end = source;
-    while ( *end && length < max_length ) ++end, ++length;
-
-    while ( source < end )
-    {
-        const char * line_end = strchr_limited( source, end, '\n' );
-        if ( !line_end ) line_end = end;
-
-        if ( node.getName() ) strcpy (last_name, node.getName());
-
-        node.setLine( source, line_end - source );
-
-        size_t indent = 0;
-        while ( source < line_end && *source <= 0x20 )
-        {
-            source++;
+    while (p < end) {
+        int indent = 0;
+        while (p < end && *p == 0x20) {
             indent++;
+            p++;
         }
-
-        if ( last_indent == ~0 ) last_indent = indent;
-
-        if ( indent > last_indent )
-        {
-            indents[num_indents++] = last_indent;
-            last_indent = indent;
-            if ( current_path[0] ) strcat (current_path, ":");
-            strcat (current_path, last_name);
-        }
-        else if ( indent < last_indent )
-        {
-            while ( last_indent > indent && num_indents )
-            {
-                last_indent = indents[--num_indents];
-                char *colon = strrchr (current_path, ':');
-                if (colon) *colon = 0;
-                else current_path[0] = 0;
+        while (num_indents > 0 && indents[num_indents-1] >= indent) {
+            char *colon = strrchr (path, ':');
+            if (colon) {
+                *colon = 0;
             }
-            last_indent = indent;
+            num_indents--;
         }
+        indents[num_indents++] = indent;
 
-        document.walkToNode( current_path ).addChild( node );
+        const char *e = p;
+        while (e < end && *e != '\n') {
+            e++;
+        }
+        if (e == p || indent == 0) {
+            path[0] = 0;
+        }
+        if (e != p) {
+            char name[e-p+1];
+            memcpy (name, p, e-p);
+            name[e-p] = 0;
 
-        source = line_end;
-        while ( *source && *source == '\n' ) source++;
+            char *colon = strrchr (name, ':');
+            if (colon) {
+                *colon = 0;
+            }
+            if (indent != 0) {
+                strcat (path, ":");
+            }
+
+            strcat (path, name);
+            if (colon) {
+                addNode (path, colon+1);
+                printf ("%s = %s\n", path, colon+1);
+            }
+            else {
+                addNode (path, NULL);
+                printf ("---- %s\n", path);
+            }
+        }
+        p = e;
+        p++;
     }
+}
+
+Bml_Parser::Bml_Node *Bml_Parser::walkToNode (const char *_path) const
+{
+    Bml_Node *node = nodes;
+
+    char *path = strdup(_path);
+
+    // split on : or [
+    char *p = path;
+    while (*p) {
+        if (*p == '[') {
+            int n = atoi(p+1) + 1;
+            char *e = p;
+            while (*e && *e != ':') {
+                e++;
+            }
+            memmove (p, e, strlen (e)+1);
+            // find n'th node starting with [p..e]
+            for (; n && node; node = node->next) {
+                size_t l = p-path;
+                if (!strncmp (node->key, path, l) && !node->key[l]) {
+                    n--;
+                }
+            }
+        }
+        p++;
+    }
+    for (; node; node = node->next) {
+        if (!strcmp (node->key, path)) {
+            free (path);
+            return node;
+        }
+    }
+    free (path);
+    return NULL;
 }
 
 const char * Bml_Parser::enumValue(const char *path) const
 {
-    return document.walkToNode(path).getValue();
+    const Bml_Node *node = walkToNode (path);
+    if (node) {
+        return node->value;
+    }
+    return NULL;
 }
 
 void Bml_Parser::setValue(const char *path, const char *value)
 {
-    document.walkToNode(path, true).setValue(value);
+    Bml_Node *node = walkToNode (path);
+    if (node) {
+        free (node->value);
+        node->value = strdup (value);
+        return;
+    }
+    addNode(path, value);
 }
 
 void Bml_Parser::setValue(const char *path, long value)
@@ -330,10 +171,10 @@ void Bml_Parser::setValue(const char *path, long value)
 
 void Bml_Parser::serialize(char *buffer, int size) const
 {
-    serialize(buffer, size, &document, 0);
+//    serialize(buffer, size, &document, 0);
 }
 
-
+#if 0
 void Bml_Parser::serialize(char *buffer, int size, Bml_Node const* node, unsigned int indent) const
 {
     size_t l = 0;
@@ -362,3 +203,4 @@ void Bml_Parser::serialize(char *buffer, int size, Bml_Node const* node, unsigne
         if ( indent == 0 ) APPEND("\n");
     }
 }
+#endif
--- plugins/gme/game-music-emu-0.6pre/gme/Bml_Parser.h
+++ plugins/gme/game-music-emu-0.6pre/gme/Bml_Parser.h
@@ -1,49 +1,25 @@
 #ifndef BML_PARSER_H
 #define BML_PARSER_H
 
-#include "blargg_common.h"
-#include <vector>
-
-class Bml_Node
-{
-    char * name;
-    char * value;
-    Bml_Node *next;
-
-    std::vector<Bml_Node> children;
-
-    static Bml_Node emptyNode;
-
-public:
-    Bml_Node();
-    Bml_Node(char const* name, size_t max_length = ~0UL);
-    Bml_Node(Bml_Node const& in);
-
-    ~Bml_Node();
-
-    void clear();
-
-    void setLine(const char * line, size_t max_length = ~0UL);
-    Bml_Node& addChild(Bml_Node const& child);
-
-    const char * getName() const;
-    const char * getValue() const;
-    
-    void setValue(char const* value);
-
-    size_t getChildCount() const;
-    Bml_Node const& getChild(size_t index) const;
-
-    Bml_Node & walkToNode( const char * path, bool use_indexes = false );
-    Bml_Node const& walkToNode( const char * path ) const;
-};
-
 class Bml_Parser
 {
-    Bml_Node document;
+    struct Bml_Node {
+        char *key;
+        char *value;
+        struct Bml_Node *next;
+    };
+
+    Bml_Node *nodes;
+    Bml_Node *tail;
+
+    void addNode (const char *key, const char *value);
+    Bml_Node *walkToNode (const char *path) const;
 
 public:
-    Bml_Parser() { }
+    Bml_Parser();
+    ~Bml_Parser();
+
+    void clearDocument ();
 
     void parseDocument(const char * document, size_t max_length = ~0UL);
 
@@ -53,8 +29,6 @@ public:
     void setValue(const char *path, const char * value);
 
     void serialize(char *out, int buffer_size) const;
-private:
-    void serialize(char *buffer, int buffer_size, Bml_Node const* node, unsigned int indent) const;
 };
 
 #endif // BML_PARSER_H
