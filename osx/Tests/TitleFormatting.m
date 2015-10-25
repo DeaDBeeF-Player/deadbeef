d64c3f89bad3122985c18346bced1dcaa236db8d
Alexey Yakovenko <wakeroid@gmail.com>
2015-10-25 17:52:19 +0100

    tf: fixed crash on NULL ctx.it (fixes #1305)

--- osx/Tests/TitleFormatting.m
+++ osx/Tests/TitleFormatting.m
@@ -411,6 +411,8 @@
     tf_eval (&ctx, bc, buffer, 1000);
     tf_free (bc);
     XCTAssert(!strcmp ("begin - 0 - end", buffer), @"The actual output is: %s", buffer);
+    tf_free (bc);
+    XCTAssert(!strcmp ("begin -  - end", buffer), @"The actual output is: %s", buffer);
     free (buffer);
 }
 
