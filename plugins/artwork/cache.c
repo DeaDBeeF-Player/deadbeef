a41138723a1c14720d412f3750de2760ef95139e
Roman Bogorodskiy <bogorodskiy@gmail.com>
2016-03-03 20:28:57 +0300

    Fix segfault in artwork cache code
    The cache_cleaner_thread() function in artwork/cache.c tries
    to opendir(3) covers_path, but return value is unchecked.
    
    Later, it calls closedir(3), and if the directory did not exist
    in the first place, NULL will be passed and it will segfault.
    
    Fix by checking if covers_dir is not NULL before calling closedir().

--- plugins/artwork/cache.c
+++ plugins/artwork/cache.c
@@ -164,7 +164,8 @@ cache_cleaner_thread (void *none)
             usleep (100000);
             deadbeef->mutex_lock (thread_mutex);
         }
-        closedir (covers_dir);
+        if (covers_dir)
+            closedir (covers_dir);
 
         /* Sleep until just after the oldest file expires */
         if (cache_expiry_seconds > 0 && !terminate) {
