748d5f0d34e55db3e89f81bdd1adaafed3b59b99
Alexey Yakovenko <wakeroid@gmail.com>
2016-04-24 22:48:15 +0200

    travis: release uploading support

--- travis/upload.sh
+++ travis/upload.sh
@@ -6,15 +6,27 @@ ssh-add travis/id_rsa || exit 1
 
 SSHOPTS="ssh -o StrictHostKeyChecking=no"
 
+VERSION=`cat PORTABLE_VERSION | perl -ne 'chomp and print'`
+
+if [[ $VERSION =~ ^[0-9]*\.[0-9]*\.[0-9]$ ]]
+then
+    # release, upload to root folder
+    echo Uploading to frs.sourceforge.net:/home/frs/project/d/de/deadbeef/
+    UPLOAD_PATH=waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/
+else
+    # not release, upload to travis folder
+    echo Uploading to /home/frs/project/d/de/deadbeef/travis/linux/$TRAVIS_BRANCH/
+    UPLOAD_PATH=waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/linux/$TRAVIS_BRANCH/
+fi
+
 case "$TRAVIS_OS_NAME" in
     linux)
         echo Uploading linux artifacts...
-        rsync -e "$SSHOPTS" deadbeef-*.tar.bz2 waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/linux/$TRAVIS_BRANCH/ || exit 1
-        rsync -e "$SSHOPTS" portable_out/build/*.tar.bz2 waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/linux/$TRAVIS_BRANCH/ || exit 1
+        rsync -e "$SSHOPTS" deadbeef-*.tar.bz2 $UPLOAD_PATH || exit 1
+        rsync -e "$SSHOPTS" portable_out/build/*.tar.bz2 $UPLOAD_PATH || exit 1
     ;;
     osx)
         echo Uploading mac artifacts...
-        VERSION=`cat PORTABLE_VERSION | perl -ne 'chomp and print'`
         rsync -e "$SSHOPTS" osx/build/Release/deadbeef-$VERSION-osx-x86_64.zip waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/osx/$TRAVIS_BRANCH/ || exit 1
     ;;
 esac
