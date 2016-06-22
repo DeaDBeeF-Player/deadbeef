3fcd77a2644b54fe8411f7436a180dce9b0fa36d
Alexey Yakovenko <wakeroid@gmail.com>
2016-06-22 08:45:51 +0200

    portable_postbuild artwork fix

--- scripts/portable_postbuild.sh
+++ scripts/portable_postbuild.sh
@@ -25,35 +25,45 @@ cp ./deadbeef $OUTDIR
 
 for i in converter pltbrowser shellexecui ; do
     if [ -f ./plugins/$i/.libs/${i}_gtk2.so ]; then
-		 cp ./plugins/$i/.libs/${i}_gtk2.so $PLUGDIR/
+        cp ./plugins/$i/.libs/${i}_gtk2.so $PLUGDIR/
+    else
+        echo ./plugins/$i/.libs/$i_gtk2.so not found
     fi
     if [ -f ./plugins/$i/.libs/${i}_gtk3.so ]; then
-		 cp ./plugins/$i/.libs/${i}_gtk3.so $PLUGDIR/
+        cp ./plugins/$i/.libs/${i}_gtk3.so $PLUGDIR/
+    else
+        echo ./plugins/$i/.libs/$i_gtk3.so not found
     fi
 
     if [ -f ./plugins/$i/.libs/$i.fallback.so ]; then
-		 cp ./plugins/$i/.libs/$i.fallback.so $PLUGDIR/
+        cp ./plugins/$i/.libs/$i.fallback.so $PLUGDIR/
     fi
 done
 
 for i in nullout cdda flac alsa mp3 hotkeys vtx \
-	 ffap ffmpeg wavpack vorbis oss vfs_curl \
-	 lastfm sid adplug sndfile artwork-legacy alac \
-	 supereq gme dumb notify musepack wildmidi \
-	 tta dca aac mms shn psf shellexec vfs_zip \
-	 m3u converter pulse dsp_libsrc mono2stereo \
-	 wma statusnotifier \
-	 ; do
+     ffap ffmpeg wavpack vorbis oss vfs_curl \
+     lastfm sid adplug sndfile alac \
+     supereq gme dumb notify musepack wildmidi \
+     tta dca aac mms shn psf shellexec vfs_zip \
+     m3u converter pulse dsp_libsrc mono2stereo \
+     wma statusnotifier \
+     ; do
     if [ -f ./plugins/$i/.libs/$i.so ]; then
-		 cp ./plugins/$i/.libs/$i.so $PLUGDIR/
-	elif [ -f ./plugins/$i/$i.so ]; then
-		 cp ./plugins/$i/$i.so $PLUGDIR/
-	elif [ -f ./plugins/$i/.libs/ddb_$i.so ]; then
-		 cp ./plugins/$i/.libs/ddb_$i.so $PLUGDIR/
-	else
-		echo ./plugins/$i/.libs/$i.so not found
-	fi
+        cp ./plugins/$i/.libs/$i.so $PLUGDIR/
+    elif [ -f ./plugins/$i/$i.so ]; then
+        cp ./plugins/$i/$i.so $PLUGDIR/
+    elif [ -f ./plugins/$i/.libs/ddb_$i.so ]; then
+        cp ./plugins/$i/.libs/ddb_$i.so $PLUGDIR/
+    else
+        echo ./plugins/$i/.libs/$i.so not found
+    fi
 done
+
+if [ -f ./plugins/artwork-legacy/.libs/artwork.so ]; then
+    cp ./plugins/artwork-legacy/.libs/artwork.so $PLUGDIR/
+else
+    echo ./plugins/artwork-legacy/.libs/artwork.so not found
+fi
 
 if [ -f ./plugins/gtkui/.libs/ddb_gui_GTK2.so ]; then
     cp ./plugins/gtkui/.libs/ddb_gui_GTK2.so $PLUGDIR/
@@ -73,12 +83,12 @@ fi
 #pixmaps
 
 for i in pause_16.png play_16.png noartwork.png buffering_16.png; do
-	cp ./pixmaps/$i $PIXMAPDIR/
+    cp ./pixmaps/$i $PIXMAPDIR/
 done
 
 # docs
 for i in ChangeLog help.txt COPYING.GPLv2 COPYING.LGPLv2.1 about.txt translators.txt; do
-	cp ./$i $DOCDIR/
+    cp ./$i $DOCDIR/
 done
 
 # icon
@@ -105,6 +115,6 @@ touch $OUTDIR/.ddb_portable
 
 # strip
 if [ $OSTYPE != 'Darwin' ];then
-	strip --strip-unneeded $OUTDIR/deadbeef
-	for i in $PLUGDIR/*.so ; do strip --strip-unneeded $i ; done
+    strip --strip-unneeded $OUTDIR/deadbeef
+    for i in $PLUGDIR/*.so ; do strip --strip-unneeded $i ; done
 fi
