ffb99f6ebd0aae9b0f65e3b695d42d46f62482f5
Alexey Yakovenko <wakeroid@gmail.com>
2016-12-26 12:53:01 +0100

    mp4parser: converted test project into cocoa app, to simplify test running

--- plugins/mp4parser/main.m
+++ plugins/mp4parser/main.m
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include "mp4parser.h"
+#import <Cocoa/Cocoa.h>
 
 int main(int argc, const char * argv[]) {
-//	mp4p_atom_t *mp4 = mp4p_open ("/Users/waker/temp/01. Tim Follin - Chronos - Title (AY & Beeper).m4a", NULL);
+	//	mp4p_atom_t *mp4 = mp4p_open ("/Users/waker/temp/01. Tim Follin - Chronos - Title (AY & Beeper).m4a", NULL);
 	mp4p_atom_t *mp4 = mp4p_open ("/Users/waker/mus/ru/Виктор Аргонов Project - 2032 - Легенда О Несбывшемся Грядущем (2007) (ALAC)/01. Виктор Аргонов Project - Увертюра.m4a", NULL);
 
 	printf ("----\n");
@@ -16,5 +17,7 @@ int main(int argc, const char * argv[]) {
 	if (mp4) {
 		mp4p_atom_free (mp4);
 	}
+
+	NSApplicationMain (argc, argv);
     return 0;
 }
