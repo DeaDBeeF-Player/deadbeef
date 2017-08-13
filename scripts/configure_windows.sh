0ce32c8d50340cbf4ac3affa0d591b626109b1e2
Jakub Wasylków <kuba_160@protonmail.com>
2017-08-13 11:36:10 +0200

    configure_windows: don't set up default prefix

--- scripts/configure_windows.sh
+++ scripts/configure_windows.sh
@@ -1,3 +1,3 @@
 ./configure --enable-maintainer-mode --disable-nullout --disable-oss --disable-sid --disable-ffap --disable-vtx --disable-adplug --disable-vorbis --disable-ffmpeg --disable-flac --disable-sndfile  --disable-wavpack --disable-cdda --disable-gme --disable-dumb --disable-musepack --disable-wildmidi --disable-tta --disable-dca --disable-aac --disable-mms --disable-shn --disable-ao --disable-supereq --disable-artwork --disable-lfm --disable-vfs-curl --disable-hotkeys --disable-notify --disable-shellexec \
 --disable-sc68 --enable-gtk2 --enable-gtk3 --enable-flac --enable-mp3 --disable-statusnotifier --enable-aac --enable-shellexec --enable-shellexecui --enable-notify --enable-artwork --enable-supereq --enable-hotkeys --enable-pulse --enable-vorbis --enable-sndfile --enable-vfs-curl --enable-nullout \
- --prefix=/usr/local $1 $2 $3
+ $1 $2 $3
