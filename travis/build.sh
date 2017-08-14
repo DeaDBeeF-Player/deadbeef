c924a012297ac65134374e6630649fab2a55cb11
Jakub Wasylków <kuba_160@protonmail.com>
2017-08-14 12:00:59 +0200

    add ppa:mingw-packages repo

--- travis/build.sh
+++ travis/build.sh
@@ -2,9 +2,10 @@ case "$TRAVIS_OS_NAME" in
     linux)
         if [ "$BUILD_WINDOWS" = "yes" ]
         then
+            sudo add-apt-repository ppa:mingw-packages/ppa
             sudo apt-get update 1> /dev/null 2> /dev/null || exit 1
             sudo apt-get install -qq autopoint automake autoconf intltool libc6-dev-i386 libc6-dev yasm libglib2.0-bin || exit 1
-            sudo apt-get install -qq binutils-mingw-w64-x86-64 gcc-mingw-w64-x86-64  win-iconv-mingw-w64-dev libz-mingw-w64-dev
+            sudo apt-get install -qq binutils-mingw-w64-x86-64 gcc-mingw-w64-x86-64 zlib-mingw-w64-cross libpthread-mingw-w64-cross
             STATICDEPS_GIT="http://github.com/kuba160/deadbeef-windows-deps"
             echo "downloading static deps..."
             git clone -q "$STATICDEPS_GIT" || exit 1
