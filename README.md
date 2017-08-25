c234c7efe8fc25d01d1d33d25639a724f6cea5af
Jakub Wasylków <kuba_160@protonmail.com>
2017-08-25 15:32:20 +0200

    Add script for installing deadbeef on Windows platform, add instruction for compiling on Windows

--- README.md
+++ README.md
@@ -36,6 +36,24 @@ The following instructions assume that commands are executed in deadbeef reposit
 * Get the output: ```osx/build/Release/deadbeef.app```
 * OR open the osx/deadbeef.xcodeproj in XCode, and build/run from there
 
+### Windows (msys2)
+
+* Install msys2
+* Get needed dependencies: 
+	```pacman -S mingw-w64-x86_64-libzip pkgconfig mingw-w64-x86_64-dlfcn mingw-w64-x86_64-libtool mingw-w64-x86_64-gcc git automake autogen autoconf gettext gettext-devel libtool make m4 tar xz intltool```
+* Get some packages for plugins:
+	```pacman -S mingw-w64-x86_64-gtk3 mingw-w64-x86_64-gtk2 mingw-w64-x86_64-libmad mingw-w64-x86_64-flac```
+* Install jansson or get compiled lib from https://github.com/kuba160/deadbeef-w64-deps
+   If you're compiling, make sure you are using mingw shell (run mingwXX.exe), if you don't want to compile it self:
+   ```git clone https://github.com/kuba160/deadbeef-w64-deps && cd deadbeef-w64-deps && make jansson-install```
+* Optionally, install other dependencies for plugins which you want to compile.
+* Switch to mingw shell (run mingwXX.exe)
+* Run ```./autogen.sh``` to bootstrap
+* Run `./scripts/configure_windows.sh`
+* `make`
+* Run `./scripts/windows_install.sh".`
+* Your build will be located in `build` folder.
+
 ----
 
 [![Support via Gratipay](https://cdn.rawgit.com/gratipay/gratipay-badge/2.3.0/dist/gratipay.png)](https://gratipay.com/deadbeef/)
---
+++ scripts/windows_install.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+. .install
+PREFIX="${PREFIX:-`pwd`/build}"
+mkdir -p $PREFIX/lib/deadbeef
+mkdir -p $PREFIX/pixmaps
+mkdir -p $PREFIX/doc
+
+rm -f $PREFIX/plugins/*.dll
+cp ./.libs/deadbeef.exe $PREFIX/
+cp ./plugins/nullout/.libs/nullout.dll $PREFIX/plugins/
+cp ./plugins/cdda/.libs/cdda.dll $PREFIX/plugins/
+cp ./plugins/flac/.libs/flac.dll $PREFIX/plugins/
+cp ./plugins/alsa/.libs/alsa.dll $PREFIX/plugins/
+cp ./plugins/sndio/.libs/sndio.dll $PREFIX/plugins/
+cp ./plugins/mp3/.libs/mp3.dll $PREFIX/plugins/
+cp ./plugins/hotkeys/.libs/hotkeys.dll $PREFIX/plugins/
+cp ./plugins/vtx/.libs/vtx.dll $PREFIX/plugins/
+cp ./plugins/ffap/.libs/ffap.dll $PREFIX/plugins/
+cp ./plugins/wavpack/.libs/wavpack.dll $PREFIX/plugins/
+cp ./plugins/vorbis/.libs/vorbis.dll $PREFIX/plugins/
+cp ./plugins/oss/.libs/oss.dll $PREFIX/plugins/
+cp ./plugins/vfs_curl/.libs/vfs_curl.dll $PREFIX/plugins/
+cp ./plugins/ffmpeg/.libs/ffmpeg.dll $PREFIX/plugins/
+cp ./plugins/lastfm/.libs/lastfm.dll $PREFIX/plugins/
+cp ./plugins/sid/.libs/sid.dll $PREFIX/plugins/
+cp ./plugins/adplug/.libs/adplug.dll $PREFIX/plugins/
+cp ./plugins/gtkui/.libs/ddb_gui_GTK2.dll $PREFIX/plugins/
+cp ./plugins/gtkui/.libs/ddb_gui_GTK3.dll $PREFIX/plugins/
+cp ./plugins/sndfile/.libs/sndfile.dll $PREFIX/plugins/
+cp ./plugins/pulse/.libs/pulse.dll $PREFIX/plugins/
+cp ./plugins/artwork/.libs/artwork.dll $PREFIX/plugins/
+cp ./plugins/supereq/.libs/supereq.dll $PREFIX/plugins/
+cp ./plugins/gme/.libs/gme.dll $PREFIX/plugins/
+cp ./plugins/dumb/.libs/ddb_dumb.dll $PREFIX/plugins/
+cp ./plugins/notify/.libs/notify.dll $PREFIX/plugins/
+cp ./plugins/musepack/.libs/musepack.dll $PREFIX/plugins/
+cp ./plugins/wildmidi/.libs/wildmidi.dll $PREFIX/plugins/
+cp ./plugins/tta/.libs/tta.dll $PREFIX/plugins/
+cp ./plugins/dca/.libs/dca.dll $PREFIX/plugins/
+cp ./plugins/aac/.libs/aac.dll $PREFIX/plugins/
+cp ./plugins/mms/.libs/mms.dll $PREFIX/plugins/
+cp ./plugins/shn/.libs/ddb_shn.dll $PREFIX/plugins/
+cp ./plugins/psf/.libs/psf.dll $PREFIX/plugins/
+cp ./plugins/shellexec/.libs/shellexec.dll $PREFIX/plugins/
+cp ./plugins/shellexecui/.libs/shellexecui_gtk2.dll $PREFIX/plugins/
+cp ./plugins/shellexecui/.libs/shellexecui_gtk3.dll $PREFIX/plugins/
+cp ./plugins/dsp_libsrc/.libs/dsp_libsrc.dll $PREFIX/plugins/
+cp ./plugins/m3u/.libs/m3u.dll $PREFIX/plugins/
+cp ./plugins/ddb_input_uade2/ddb_input_uade2.dll $PREFIX/plugins/
+cp ./plugins/converter/.libs/converter.dll $PREFIX/plugins/
+cp ./plugins/converter/.libs/converter_gtk2.dll $PREFIX/plugins/
+cp ./plugins/converter/.libs/converter_gtk3.dll $PREFIX/plugins/
+cp ./plugins/soundtouch/ddb_soundtouch.dll $PREFIX/plugins/
+cp ./plugins/vfs_zip/.libs/vfs_zip.dll $PREFIX/plugins/
+cp ./plugins/medialib/.libs/medialib.dll $PREFIX/plugins/
+cp ./plugins/mono2stereo/.libs/ddb_mono2stereo.dll $PREFIX/plugins/
+cp ./plugins/alac/.libs/alac.dll $PREFIX/plugins/
+cp ./plugins/wma/.libs/wma.dll $PREFIX/plugins/
+cp ./plugins/pltbrowser/.libs/pltbrowser_gtk2.dll $PREFIX/plugins/
+cp ./plugins/pltbrowser/.libs/pltbrowser_gtk3.dll $PREFIX/plugins/
+cp ./plugins/coreaudio/.libs/coreaudio.dll $PREFIX/plugins/
+cp ./plugins/sc68/.libs/in_sc68.dll $PREFIX/plugins/
+cp ./plugins/statusnotifier/.libs/statusnotifier.dll $PREFIX/plugins/
+
+# libs
+cp $(ldd plugins/*/.libs/*.dll .libs/deadbeef.exe | awk 'NF == 4 {print $3}; NF == 2 {print $1}' |grep -i -v "System32" | grep -i -v "WinSxS" |sort -u) $PREFIX/
+
+# pixmaps
+for i in pause_16.png play_16.png noartwork.png buffering_16.png; do
+    cp ./pixmaps/$i $PREFIX/pixmaps/
+done
+
+# docs
+for i in ChangeLog help.txt COPYING.GPLv2 COPYING.LGPLv2.1 about.txt translators.txt; do
+    cp ./$i $PREFIX/doc/
+done
+
+# icon
+cp ./icons/32x32/deadbeef.png $PREFIX/
+
+# converter presets
+cp -r plugins/converter/convpresets $PREFIX/plugins/
+
+# sc68data
+cp -r plugins/sc68/.libs/in_sc68.so $PREFIX/plugins/
+mkdir -p  $PREFIX/plugins/data68/Replay
+cp -r plugins/sc68/file68/data68/Replay/*.bin $PREFIX/plugins/data68/Replay/
+
+# translations
+mkdir -p $PREFIX/locale
+for i in po/*.gmo ; do
+    base=`basename po/$i .gmo`
+    mkdir -p $PREFIX/locale/$base/LC_MESSAGES
+    cp $i $PREFIX/locale/$base/LC_MESSAGES/deadbeef.mo
+done
+cp translation/help.pt_BR.txt $PREFIX/doc/
+cp translation/help.ru.txt $PREFIX/doc/
+cp translation/help.zh_TW.txt $PREFIX/doc/
+
+# strip
+strip --strip-unneeded $PREFIX/deadbeef.exe
+for i in $PREFIX/plugins/*.dll ; do strip --strip-unneeded $i ; done
