a14ef04599aaf39ac909b5328e3d89fc929ecda8
Alexey Yakovenko <wakeroid@gmail.com>
2017-09-29 11:57:23 +0200

    alsa: fixed wrong format setting error messages

--- plugins/alsa/alsa.c
+++ plugins/alsa/alsa.c
@@ -23,8 +23,7 @@
 #include "../../deadbeef.h"
 #include "../../config.h"
 
-//#define trace(...) { fprintf(stderr, __VA_ARGS__); }
-#define trace(fmt,...)
+#define trace(...) { deadbeef->log_detailed (&plugin.plugin, 0, __VA_ARGS__); }
 
 #define min(x,y) ((x)<(y)?(x):(y))
 
@@ -165,7 +164,7 @@ retry:
     }
 
     if ((err = snd_pcm_hw_params_set_format (audio, hw_params, sample_fmt)) < 0) {
-        fprintf (stderr, "cannot set sample format (%s), trying all supported formats\n", snd_strerror (err));
+        fprintf (stderr, "cannot set sample format to %d bps (error: %s), trying all supported formats\n", plugin.fmt.bps, snd_strerror (err));
 
         int fmt_cnt[] = { 16, 24, 32, 32, 8 };
 #if WORDS_BIGENDIAN
@@ -179,7 +178,8 @@ retry:
         for (i = 0; fmt[i] != -1; i++) {
             if (fmt[i] != sample_fmt && fmt_cnt[i] > plugin.fmt.bps) {
                 if (snd_pcm_hw_params_set_format (audio, hw_params, fmt[i]) >= 0) {
-                    fprintf (stderr, "cannot set sample format (%s), trying all supported formats\n", snd_strerror (err));
+                    fprintf (stderr, "Found compatible format %d bps\n", fmt_cnt[i]);
+                    sample_fmt = fmt[i];
                     break;
                 }
             }
@@ -190,7 +190,8 @@ retry:
             for (i = 0; fmt[i] != -1; i++) {
                 if (fmt[i] != sample_fmt && fmt_cnt[i] < plugin.fmt.bps) {
                     if (snd_pcm_hw_params_set_format (audio, hw_params, fmt[i]) >= 0) {
-                        fprintf (stderr, "cannot set sample format (%s), trying all supported formats\n", snd_strerror (err));
+                        fprintf (stderr, "Found compatible format %d bps\n", fmt_cnt[i]);
+                        sample_fmt = fmt[i];
                         break;
                     }
                 }
@@ -198,6 +199,7 @@ retry:
         }
 
         if (fmt[i] == -1) {
+            fprintf (stderr, "Fallback format could not be found\n");
             goto error;
         }
     }
@@ -762,6 +764,7 @@ static DB_output_t plugin = {
     .plugin.version_major = 1,
     .plugin.version_minor = 0,
     .plugin.type = DB_PLUGIN_OUTPUT,
+//    .plugin.flags = DDB_PLUGIN_FLAG_LOGGING,
     .plugin.id = "alsa",
     .plugin.name = "ALSA output plugin",
     .plugin.descr = "plays sound through linux standard alsa library",
