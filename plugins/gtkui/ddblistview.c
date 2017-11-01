04596d408c00a9fe1b24be6011f0f67ae06a6492
Alexey Yakovenko <wakeroid@gmail.com>
2017-11-01 16:07:49 +0100

    gtkui: removed a very wrong fill_list_background call, which was breaking playlist rendering

--- plugins/gtkui/ddblistview.c
+++ plugins/gtkui/ddblistview.c
@@ -872,7 +872,6 @@ ddb_listview_list_render (DdbListview *listview, cairo_t *cr, GdkRectangle *clip
 
     draw_begin (&listview->listctx, cr);
     draw_begin (&listview->grpctx, cr);
-    fill_list_background(listview, cr, scrollx, -listview->scrollpos, total_width, max(listview->fullheight, listview->list_height), clip);
 
     // find 1st group
     DdbListviewGroup *grp = listview->groups;
