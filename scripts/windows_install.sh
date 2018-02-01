64d8ea95c295e80d50b432f90dda076360802109
Jakub Wasylków <kuba_160@protonmail.com>
2018-02-01 23:28:16 +0100

    windows_install.sh: fix typo

--- scripts/windows_install.sh
+++ scripts/windows_install.sh
@@ -105,7 +105,7 @@ cp translation/help.zh_TW.txt $PREFIX/doc/
 
 # strip
 strip --strip-unneeded $PREFIX/deadbeef.exe
-for i in $PREFIX/plugins/.libs/*.dll ; do strip --strip-unneeded $i ; done
+for i in $PREFIX/plugins/*.dll ; do strip --strip-unneeded $i ; done
 
 # MS-Windows theme (GTK2)
 for i in /mingw32 /mingw64 /usr; do
