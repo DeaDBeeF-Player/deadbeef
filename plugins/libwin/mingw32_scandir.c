9df87712c04594f09a1e3ffcccb7aa5b1bcf97e2
Jakub Wasylków <kuba_160@protonmail.com>
2018-02-02 18:47:16 +0100

    libwin: fix dependency issue

--- plugins/libwin/mingw32_scandir.c
+++ plugins/libwin/mingw32_scandir.c
@@ -3,8 +3,39 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
-#include "../../deadbeef.h"
-extern DB_functions_t *deadbeef;
+#include <iconv.h>
+
+// taken and modified from ../../junklib.c
+int
+junk_iconv2 (const char *in, int inlen, char *out, int outlen, const char *cs_in, const char *cs_out) {
+// NOTE: this function must support utf8->utf8 conversion, used for validation
+    iconv_t cd = iconv_open (cs_out, cs_in);
+    if (cd == (iconv_t)-1) {
+        return -1;
+    }
+#if defined(__linux__) || defined(__OpenBSD__)
+    char *pin = (char*)in;
+#else
+    const char *pin = in;
+#endif
+
+    size_t inbytesleft = inlen;
+    size_t outbytesleft = outlen;
+
+    char *pout = out;
+
+    size_t res = iconv (cd, (char **) &pin, &inbytesleft, &pout, &outbytesleft);
+    int err = errno;
+    iconv_close (cd);
+
+    //trace ("iconv -f %s -t %s '%s': returned %d, inbytes %d/%d, outbytes %d/%d, errno=%d\n", cs_in, cs_out, in, (int)res, inlen, (int)inbytesleft, outlen, (int)outbytesleft, err);
+    if (res == -1) {
+        return -1;
+    }
+    out[pout-out] = 0;
+    //trace ("iconv out: %s (len=%d)\n", out, pout - out);
+    return pout - out;
+}
 
 int scandir (const char      *dirname,
              struct dirent ***namelist_to,
@@ -18,7 +49,7 @@ int scandir (const char      *dirname,
     WIN32_FIND_DATAW fData;
     int dirname_w_len = strlen(dirname) * 2 + 8;
     wchar_t dirname_w[dirname_w_len];
-    int iconv_ret = deadbeef->junk_iconv (dirname, strlen(dirname) + 1, (char *) dirname_w, dirname_w_len, "UTF-8", "WCHAR_T");
+    int iconv_ret = junk_iconv2 (dirname, strlen(dirname) + 1, (char *) dirname_w, dirname_w_len, "UTF-8", "WCHAR_T");
     wcscat (dirname_w,L"\\*.*");
     HANDLE hFind = FindFirstFileW (dirname_w, &fData);
     if (INVALID_HANDLE_VALUE == hFind) {
@@ -41,7 +72,7 @@ int scandir (const char      *dirname,
                     size_t len_l = wcslen (fData.cFileName)*2;
                     size_t len_r = len_l * 2;
                     char string_tmp[len_r];
-                    int ret = deadbeef->junk_iconv ((char *) fData.cFileName, len_l, string_tmp, len_r, "WCHAR_T", "UTF-8");
+                    int ret = junk_iconv2 ((char *) fData.cFileName, len_l, string_tmp, len_r, "WCHAR_T", "UTF-8");
                     if (ret == -1) {
                         free (tmp);
                     }
