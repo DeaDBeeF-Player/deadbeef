8ec51763c6d52c6a0fc824e76793f93302c7c4b6
Jakub Wasylków <kuba_160@protonmail.com>
2018-02-02 18:48:50 +0100

    configure_windows.sh: reorganize and update

--- scripts/configure_windows.sh
+++ scripts/configure_windows.sh
@@ -1,3 +1,10 @@
-./configure --enable-maintainer-mode --disable-nullout --disable-oss --disable-sid --disable-ffap --disable-vtx --disable-adplug --disable-vorbis --disable-ffmpeg --disable-flac --disable-sndfile  --disable-wavpack --disable-cdda --disable-gme --disable-dumb --disable-musepack --disable-wildmidi --disable-tta --disable-dca --disable-aac --disable-mms --disable-shn --disable-ao --disable-supereq --disable-artwork --disable-lfm --disable-vfs-curl --disable-hotkeys --disable-notify --disable-shellexec \
---disable-sc68 --enable-gtk2 --enable-gtk3 --enable-flac --enable-mp3 --disable-statusnotifier --enable-aac --enable-shellexec --enable-shellexecui --enable-notify --enable-artwork --enable-supereq --enable-hotkeys --enable-pulse --enable-vorbis --enable-sndfile --enable-vfs-curl --enable-nullout --disable-psf \
+./configure --enable-maintainer-mode \
+--disable-oss --disable-pulse --disable-sid --disable-ffap --disable-vtx --disable-adplug --disable-wavpack --disable-gme --disable-dumb --disable-musepack --disable-wildmidi \
+--disable-tta --disable-dca --disable-shn --disable-supereq --disable-lfm --disable-notify --disable-shellexec --disable-sc68 --disable-psf \
+--enable-gtk2 --enable-gtk3 \
+--enable-nullout --enable-portaudio --enable-waveout \
+--enable-flac --enable-mp3 --enable-aac --enable-vorbis --enable-wma --enable-sndfile \
+--enable-shellexec --enable-shellexecui --enable-notify --enable-supereq --enable-hotkeys \
+--enable-vfs-curl \
+--disable-ffmpeg --disable-cdda --disable-mms --disable-artwork \
  $1 $2 $3
