8faecdbc8365659d8763a4295a6b61ae0f6a73b8
Alexey Yakovenko <wakeroid@gmail.com>
2018-05-15 22:42:34 +0200

    travis: disable i686 build

--- travis/build.sh
+++ travis/build.sh
@@ -12,9 +12,9 @@ case "$TRAVIS_OS_NAME" in
         echo "installing the needed build dependencies..."
         sudo apt-get update 1> /dev/null 2> /dev/null || exit 1
         sudo apt-get install -qq autopoint automake autoconf intltool libc6-dev-i386 libc6-dev yasm libglib2.0-bin libx11-dev || exit 1
-        echo "building for i686"
-        ARCH=i686 ./scripts/static_build.sh || exit 1
-        ARCH=i686 ./scripts/portable_package_static.sh || exit 1
+#        echo "building for i686"
+#        ARCH=i686 ./scripts/static_build.sh || exit 1
+#        ARCH=i686 ./scripts/portable_package_static.sh || exit 1
         echo "building for x86_64"
         ARCH=x86_64 ./scripts/static_build.sh || exit 1
         ARCH=x86_64 ./scripts/portable_package_static.sh || exit 1
