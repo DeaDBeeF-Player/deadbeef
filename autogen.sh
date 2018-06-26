094677a30960db8e0653be73f5200170fa636354
orbea <orbea@fredslev.dk>
2018-06-26 10:46:55 -0700

    configure.ac: Fix autoreconf.
    This allows removing some intl files from Makefile.am and the
    included po/Makevars file is default except for the addition of:
      --from-code=UTF-8
    
    This also silences the warnings from autogen.sh.
    
    configure.ac:20: warning: The 'AM_PROG_MKDIR_P' macro is deprecated, and its use is discouraged.
    configure.ac:20: You should use the Autoconf-provided 'AC_PROG_MKDIR_P' macro instead,
    configure.ac:20: and use '$(MKDIR_P)' instead of '$(mkdir_p)'in your Makefile.am files.
    
    v2: Update gettext version to 0.19.2 to avoid gettext bugs in
    'make distcheck'.

--- autogen.sh
+++ autogen.sh
@@ -5,4 +5,3 @@ echo "Running autoheader..."; autoheader || exit 1
 echo "Running autoconf..."; autoconf || exit 1
 echo "Running libtoolize..."; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
 echo "Running automake..."; automake --add-missing --copy --gnu || exit 1
-echo "Running intltoolize"; intltoolize --force --automake
