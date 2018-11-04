20342649a620a72a0161008eb4d6588591ba693e
Alexey Yakovenko <wakeroid@gmail.com>
2018-11-04 13:26:50 +0100

    fix a few build issues in docker

--- tools/docker-build/docker-build.sh
+++ tools/docker-build/docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker build -t deadbeef -f tools/docker-build/Dockerfile .
-mkdir -p docker-build
+mkdir -p docker-artifacts
 docker run -v ${PWD}/docker-artifacts:/usr/src/deadbeef/portable deadbeef
