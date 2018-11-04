327273d490bdaa103ad6228c06b4141104c8f80d
Alexey Yakovenko <wakeroid@gmail.com>
2018-11-04 13:46:13 +0100

    revert autogen.sh change

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-echo "Running libtoolize..."; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
-echo "Running intltoolize"; intltoolize --force --automake
 echo "Running autopoint..."; autopoint -f || :
+echo "Running aclocal..."; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
 echo "Running autoheader..."; autoheader || exit 1
 echo "Running autoconf..."; autoconf || exit 1
-echo "Running aclocal..."; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
+echo "Running libtoolize..."; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
 echo "Running automake..."; automake --add-missing --copy --gnu || exit 1
+echo "Running intltoolize"; intltoolize --force --automake
