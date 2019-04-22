d0a160236b11fd9a65ec8fa397a89698444d575c
Alexey Yakovenko <wakeroid@gmail.com>
2019-04-22 19:40:19 +0200

    portaudio: property title fix

---
+++ plugins/cocoaui/Scriptable.h
@@ -0,0 +1,17 @@
+//
+//  Scriptable.h
+//  DeaDBeeF
+//
+//  Created by Alexey Yakovenko on 4/22/19.
+//  Copyright © 2019 Alexey Yakovenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Scriptable : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
