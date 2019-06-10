cf3d01594561b220388c5916370ad317c2eb540d
Alexey Yakovenko <wakeroid@gmail.com>
2019-06-10 17:45:24 +0200

    gtkui: reset splitter ratio to 0.5 if not present (fixes #2139)

--- plugins/gtkui/widgets.c
+++ plugins/gtkui/widgets.c
@@ -100,7 +100,6 @@ typedef struct {
     // size of second child
     int size2;
     float ratio;
-    int got_ratio;
     int locked;
 } w_splitter_t;
 
@@ -1192,6 +1191,10 @@ w_splitter_load (struct ddb_gtkui_widget_s *w, const char *type, const char *s) 
         return NULL;
     }
 
+    w_splitter_t *sp = (w_splitter_t *)w;
+
+    int got_ratio = 0;
+
     char key[MAX_TOKEN], val[MAX_TOKEN];
     for (;;) {
         get_keyvalue (s,key,val);
@@ -1200,8 +1203,14 @@ w_splitter_load (struct ddb_gtkui_widget_s *w, const char *type, const char *s) 
             ((w_splitter_t *)w)->locked = atoi (val);
         }
         else if (!strcmp (key, "ratio")) {
-            ((w_splitter_t *)w)->ratio = atof (val);
-            ((w_splitter_t *)w)->got_ratio = 1;
+            sp->ratio = atof (val);
+            if (sp->ratio < 0) {
+                sp->ratio = 0;
+            }
+            if (sp->ratio > 1) {
+                sp->ratio = 1;
+            }
+            got_ratio = 1;
         }
         else if (!strcmp (key, "pos")) {
             ((w_splitter_t *)w)->size1 = atoi (val);
@@ -1209,6 +1218,10 @@ w_splitter_load (struct ddb_gtkui_widget_s *w, const char *type, const char *s) 
         else if (!strcmp (key, "size2")) {
             ((w_splitter_t *)w)->size2 = atoi (val);
         }
+    }
+
+    if (!got_ratio) {
+        sp->ratio = 0.5;
     }
 
     return s;
@@ -1356,19 +1369,7 @@ void
 w_splitter_init (ddb_gtkui_widget_t *base) {
     w_splitter_t *w = (w_splitter_t *)base;
 
-    if (!w->got_ratio) { // migration from pre-1.8
-        GtkAllocation a;
-        gtk_widget_get_allocation(w->box, &a);
-        if (w->size1 > 0) {
-            w->locked = DDB_SPLITTER_SIZE_MODE_LOCK_C1;
-        }
-        else if (w->size2 > 0) {
-            w->locked = DDB_SPLITTER_SIZE_MODE_LOCK_C2;
-        }
-    }
-    else {
-        ddb_splitter_set_proportion (DDB_SPLITTER (w->box), w->ratio);
-    }
+    ddb_splitter_set_proportion (DDB_SPLITTER (w->box), w->ratio);
     ddb_splitter_set_size_mode (DDB_SPLITTER (w->box), w->locked);
     if (w->locked == DDB_SPLITTER_SIZE_MODE_LOCK_C1) {
         ddb_splitter_set_child1_size (DDB_SPLITTER (w->box), w->size1);
