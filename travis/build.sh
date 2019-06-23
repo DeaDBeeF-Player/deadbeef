5797057df7f13b8bc96818c18f2e5db2c094a628
Alexey Yakovenko <wakeroid@gmail.com>
2019-06-23 20:08:18 +0200

    enable building packages on travis-ci

--- travis/build.sh
+++ travis/build.sh
@@ -19,6 +19,10 @@ case "$TRAVIS_OS_NAME" in
         echo "building for x86_64"
         ARCH=x86_64 ./scripts/static_build.sh || exit 1
         ARCH=x86_64 ./scripts/portable_package_static.sh || exit 1
+        echo "making deb package"
+        ARCH=x86_64 ./tools/packages/debian.sh || exit 1
+        echo "making arch package"
+        ARCH=x86_64 ./tools/packages/arch.sh || exit 1
         echo "running make dist"
         make dist || exit 1
     ;;
