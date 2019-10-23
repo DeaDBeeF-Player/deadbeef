22323a6ac001ebda40f429058b56aa703258e448
Alexey Yakovenko <wakeroid@gmail.com>
2019-10-23 19:11:15 +0200

    aac: fix crash caused by closing an already closed file handle (fixes #2246)

--- plugins/aac/aac.c
+++ plugins/aac/aac.c
@@ -1006,6 +1006,7 @@ aac_insert (ddb_playlist_t *plt, DB_playItem_t *after, const char *fname) {
 
                     int64_t fsize = deadbeef->fgetlength (fp);
                     deadbeef->fclose (fp);
+                    fp = NULL;
 
                     char s[100];
                     snprintf (s, sizeof (s), "%lld", fsize);
@@ -1052,12 +1053,13 @@ aac_insert (ddb_playlist_t *plt, DB_playItem_t *after, const char *fname) {
                 }
             }
             mp4ff_close (mp4);
-            if (i < ntracks) {
+            if (fp) {
                 deadbeef->fclose (fp);
+            }
+            if (i < ntracks) {
                 return after;
             }
             // mp4 container found, but no valid aac tracks in it
-            deadbeef->fclose (fp);
             return NULL;
         }
     }
