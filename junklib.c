1872828ca00a11a871c92119fa27d609590c0313
Alexey Yakovenko <wakeroid@gmail.com>
2019-12-24 13:51:22 +0100

    junklib: convert large macro into a function

--- junklib.c
+++ junklib.c
@@ -1234,6 +1234,30 @@ junk_id3v1_read (playItem_t *it, DB_FILE *fp) {
     return junk_id3v1_read_int (it, id3, NULL);
 }
 
+
+static void
+id3v2_conv(playItem_t *it, const char *name, char store[], size_t store_len, const char *enc) {
+    memset (store, 0x20, store_len);
+    const char *meta = pl_find_meta (it, name);
+    if (meta) {
+        char temp[1000];
+        int l = junk_iconv (meta, (int)strlen (meta), temp, (int)sizeof (temp), UTF8_STR, enc);
+        if (l == -1) {
+            memset (store, 0, store_len);
+        }
+        else {
+            strncpy (store, temp, store_len);
+        }
+        for (char *cr = store; cr < store+store_len; cr++) {
+            if (*cr == '\n') {
+                *cr = 0;
+                break;
+            }
+        }
+    }
+}
+
+
 int
 junk_id3v1_write2 (int fd, playItem_t *it, const char *enc) {
     char title[30] = "";
@@ -1248,34 +1272,11 @@ junk_id3v1_write2 (int fd, playItem_t *it, const char *enc) {
 
     pl_lock ();
 
-#define conv(name, store) {\
-    memset (store, 0x20, sizeof (store));\
-    meta = pl_find_meta (it, name);\
-    if (meta) {\
-        char temp[1000];\
-        int l = junk_iconv (meta, (int)strlen (meta), temp, (int)sizeof (temp), UTF8_STR, enc);\
-        if (l == -1) {\
-            memset (store, 0, sizeof (store));\
-        }\
-        else {\
-            strncpy (store, temp, sizeof (store));\
-        }\
-        for (char *cr = store; cr < store+sizeof(store); cr++) {\
-            if (*cr == '\n') {\
-                *cr = 0;\
-                break;\
-            }\
-        }\
-    }\
-}
-
-    conv ("title", title);
-    conv ("artist", artist);
-    conv ("album", album);
-    conv ("year", year);
-    conv ("comment", comment);
-
-#undef conv
+    id3v2_conv (it, "title", title, sizeof (title), enc);
+    id3v2_conv (it, "artist", artist, sizeof (artist), enc);
+    id3v2_conv (it, "album", album, sizeof (album), enc);
+    id3v2_conv (it, "year", year, sizeof (year), enc);
+    id3v2_conv (it, "comment", comment, sizeof (comment), enc);
 
     // tracknum
     meta = pl_find_meta (it, "track");
