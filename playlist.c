07bf697de142970af89ee8d82a84b7b24adb5d4b
Jakub Wasylków <kuba_160@protonmail.com>
2020-02-23 20:07:19 +0100

    Revert "playlist: check for dirent d_type extension directly"
    This reverts commit 13752be004b8e633108e3e5279d3cf059362ff2f.

--- playlist.c
+++ playlist.c
@@ -1202,7 +1202,7 @@ plt_insert_dir_int (int visibility, playlist_t *playlist, DB_vfs_t *vfs, playIte
         n = vfs->scandir (dirname, &namelist, NULL, dirent_alphasort);
         // we can't rely on vfs plugins to set d_type
         // windows/svr4 unixes: missing dirent[]->d_type
-        #ifdef _DIRENT_HAVE_D_TYPE
+        #if !defined(__MINGW32__) && !defined(__SVR4)
         for (int i = 0; i < n; i++) {
             namelist[i]->d_type = DT_REG;
         }
