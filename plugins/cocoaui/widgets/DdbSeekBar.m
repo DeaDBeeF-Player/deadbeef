47f403634c53e71c01d2166e52365435ca8b524d
Alexey Yakovenko <wakeroid@gmail.com>
2020-03-10 22:06:11 +0100

    cocoaui: a new seekbar (experimental)

--- plugins/cocoaui/widgets/DdbSeekBar.m
+++ plugins/cocoaui/widgets/DdbSeekBar.m
@@ -29,7 +29,17 @@
 @property (nonatomic,readwrite) BOOL dragging;
 
 @property (nonatomic) CALayer *track;
+@property (nonatomic) CALayer *trackPos;
 @property (nonatomic) CALayer *thumb;
+
+@property (nonatomic) NSColor *trackBackgroundColor;
+@property (nonatomic) NSColor *trackInactiveBackgroundColor;
+@property (nonatomic) NSColor *trackPosBackgroundColor;
+@property (nonatomic) NSColor *trackPosInactiveBackgroundColor;
+@property (nonatomic) NSColor *thumbBackgroundColor;
+@property (nonatomic) NSColor *thumbBorderColor;
+@property (nonatomic) NSColor *thumbInactiveBackgroundColor;
+@property (nonatomic) NSColor *thumbInactiveBorderColor;
 
 @end
 
@@ -61,17 +71,31 @@
     self.layer.delegate = self;
 
     self.track = [CALayer new];
+    self.trackPos = [CALayer new];
     self.thumb = [CALayer new];
 
-    self.track.borderWidth = 2;
-    self.track.borderColor = [NSColor.alternateSelectedControlColor colorWithAlphaComponent:0.8].CGColor;
-    self.track.cornerRadius = 5;
+    self.trackBackgroundColor = [NSColor.whiteColor shadowWithLevel:0.4];
+    self.trackInactiveBackgroundColor = [NSColor.whiteColor shadowWithLevel:0.2];
+    self.trackPosBackgroundColor = [NSColor.whiteColor shadowWithLevel:0.6];
+    self.trackPosInactiveBackgroundColor = [NSColor.whiteColor shadowWithLevel:0.4];
+    self.thumbBackgroundColor = [NSColor.alternateSelectedControlColor highlightWithLevel:0.2];
+    self.thumbInactiveBackgroundColor = [NSColor.whiteColor shadowWithLevel:0.4];
+    self.thumbBorderColor = [NSColor.whiteColor shadowWithLevel:0.1];
+    self.thumbInactiveBorderColor = [NSColor.whiteColor shadowWithLevel:0.1];
 
-    self.thumb.backgroundColor = [NSColor.alternateSelectedControlColor colorWithAlphaComponent:0.8].CGColor;
-    self.thumb.cornerRadius = 5;
+    self.track.backgroundColor = self.trackBackgroundColor.CGColor;
+    self.track.cornerRadius = 2;
+    self.trackPos.backgroundColor = self.trackPosBackgroundColor.CGColor;
+    self.trackPos.cornerRadius = 2;
 
-    [self.layer addSublayer:self.thumb];
+    self.thumb.backgroundColor = self.thumbBackgroundColor.CGColor;
+    self.thumb.borderColor = self.thumbBorderColor.CGColor;
+    self.thumb.borderWidth = 1;
+    self.thumb.cornerRadius = 3;
+
     [self.layer addSublayer:self.track];
+    [self.layer addSublayer:self.trackPos];
+    [self.layer addSublayer:self.thumb];
 
     [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(becameKey:) name:NSWindowDidBecomeKeyNotification object:nil];
     [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(resignedKey:) name:NSWindowDidResignKeyNotification object:nil];
@@ -80,16 +104,20 @@
 - (void)becameKey:(NSNotification *)notification {
     [CATransaction begin];
     [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
-    self.track.borderColor = [NSColor.alternateSelectedControlColor colorWithAlphaComponent:0.8].CGColor;
-    self.thumb.backgroundColor = [NSColor.alternateSelectedControlColor colorWithAlphaComponent:0.8].CGColor;
+    self.track.backgroundColor = self.trackBackgroundColor.CGColor;
+    self.trackPos.backgroundColor = self.trackPosBackgroundColor.CGColor;
+    self.thumb.backgroundColor = self.thumbBackgroundColor.CGColor;
+    self.thumb.borderColor = self.thumbBorderColor.CGColor;
     [CATransaction commit];
 }
 
 - (void)resignedKey:(NSNotification *)notification {
     [CATransaction begin];
     [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
-    self.track.borderColor = NSColor.controlShadowColor.CGColor;
-    self.thumb.backgroundColor = NSColor.controlShadowColor.CGColor;
+    self.track.backgroundColor = self.trackInactiveBackgroundColor.CGColor;
+    self.trackPos.backgroundColor = self.trackPosInactiveBackgroundColor.CGColor;
+    self.thumb.backgroundColor = self.thumbInactiveBackgroundColor.CGColor;
+    self.thumb.borderColor = self.thumbInactiveBorderColor.CGColor;
     [CATransaction commit];
 }
 
@@ -97,17 +125,18 @@
     if (layer == self.layer) {
         [CATransaction begin];
         [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
-        CGFloat y = (NSHeight(self.frame)-10)/2;
-        self.track.frame = NSMakeRect(0, y, NSWidth(self.frame), 10);
+        CGFloat y = NSHeight(self.frame)/2;
+        self.track.frame = NSMakeRect(3, y-1.5, NSWidth(self.frame)-6, 3);
         [self layoutThumbLayer];
         [CATransaction commit];
     }
 }
 
 - (void)layoutThumbLayer {
-    CGFloat y = (NSHeight(self.frame)-10)/2;
-    CGFloat w = self.floatValue / 100 * NSWidth(self.frame);
-    self.thumb.frame = NSMakeRect(0, y, w, 10);
+    CGFloat y = NSHeight(self.frame)/2;
+    CGFloat x = self.floatValue / 100 * (NSWidth(self.frame)-6);\
+    self.thumb.frame = NSMakeRect(x, y-6, 6, 12);
+    self.trackPos.frame = NSMakeRect(3, y-1.5, x, 3);
 }
 
 - (void)setFloatValue:(float)floatValue {
@@ -122,7 +151,7 @@
 - (void)updateThumb:(NSEvent * _Nonnull)theEvent {
     CGFloat pos = [self convertPoint:theEvent.locationInWindow fromView:nil].x;
 
-    pos = pos/NSWidth(self.frame)*100;
+    pos = (pos-3)/(NSWidth(self.frame)-6)*100;
     pos = MAX(0, MIN(100, pos));
     self.floatValue = pos;
 
