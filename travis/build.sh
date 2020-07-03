f40588368fc82ab3b52eed4664414597058d1aec
Alexey Yakovenko <wakeroid@gmail.com>
2020-07-03 20:31:11 +0200

    travis: verbose xcodebuild log

--- travis/build.sh
+++ travis/build.sh
@@ -32,8 +32,8 @@ case "$TRAVIS_OS_NAME" in
         gem install xcpretty 1> /dev/null 2> /dev/null || exit 1
         rev=`git rev-parse --short HEAD`
         /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $rev"  plugins/cocoaui/deadbeef-Info.plist
-        xcodebuild -project osx/deadbeef.xcodeproj -target DeaDBeeF -configuration Release -quiet | xcpretty ; test ${PIPESTATUS[0]} -eq 0 || exit 1
-        xcodebuild test -project osx/deadbeef.xcodeproj -scheme deadbeef -configuration Release -quiet | xcpretty ; test ${PIPESTATUS[0]} -eq 0 || exit 1
+        xcodebuild -project osx/deadbeef.xcodeproj -target DeaDBeeF -configuration Release || exit 1
+        xcodebuild test -project osx/deadbeef.xcodeproj -scheme deadbeef -configuration Release || exit 1
         VERSION=`cat PORTABLE_VERSION | perl -ne 'chomp and print'`
         cd osx/build/Release
         zip -r deadbeef-$VERSION-osx-x86_64.zip DeaDBeeF.app || exit 1
