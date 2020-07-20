0952446d21ce81830ddfab7e4ba6712c91d6beba
Alexey Yakovenko <wakeroid@gmail.com>
2020-07-20 21:28:04 +0200

    cocoaui: move playback buttons to the sidebar section

--- plugins/cocoaui/SplitViewMainWindow/SidebarSplitViewController.m
+++ plugins/cocoaui/SplitViewMainWindow/SidebarSplitViewController.m
@@ -10,12 +10,15 @@
 
 @interface SidebarSplitViewController ()
 
-@property IBOutlet NSViewController* sidebarViewController ;
-@property IBOutlet NSViewController* bodyViewController ;
+@property IBOutlet NSViewController* sidebarViewController;
+@property IBOutlet NSViewController* bodyViewController;
 
-@property IBOutlet NSSplitView* splitView ;
+@property IBOutlet NSSplitView* splitView;
+
+@property (nonatomic) id trackingItem;
 
 @end
+
 
 @implementation SidebarSplitViewController
 
@@ -31,6 +34,12 @@
     NSSplitViewItem* bodyItem = [NSSplitViewItem splitViewItemWithViewController:self.bodyViewController];
     bodyItem.canCollapse = YES;
     [self insertSplitViewItem:bodyItem atIndex:1];
+
+    if (@available(macOS 10.16, *)) {
+        self.trackingItem = [NSTrackingSeparatorToolbarItem trackingSeparatorToolbarItemWithIdentifier:NSToolbarSidebarTrackingSeparatorItemIdentifier splitView:self.splitView dividerIndex:0];
+
+        [self.view.window.toolbar insertItemWithItemIdentifier:NSToolbarSidebarTrackingSeparatorItemIdentifier atIndex:1];
+    }
 
     [super viewDidLoad];
 }
