63919eff79cae9123ae9e9a71e85fd7ba14b7b25
Jakub Wasylków <kuba_160@protonmail.com>
2020-09-08 21:12:35 +0200

    travis: cancel windows build if make fails

--- travis/build.sh
+++ travis/build.sh
@@ -39,8 +39,8 @@ case "$TRAVIS_OS_NAME" in
         git clone https://github.com/kuba160/deadbeef-windows-deps.git
         wget https://github.com/premake/premake-core/releases/download/v5.0.0-alpha15/premake-5.0.0-alpha15-windows.zip && unzip premake-5.0.0-alpha15-windows.zip
         $mingw64 ./premake5 --os=linux --file=premake5-win.lua --standard gmake
-        $mingw64 make config=release_windows
-        $mingw64 make config=debug_windows
+        $mingw64 make config=release_windows || exit 1
+        $mingw64 make config=debug_windows || exit 1
         cp -r deadbeef-windows-deps/Windows-10 bin/debug/share/themes/Windows-10
         cp -r deadbeef-windows-deps/Windows-10 bin/release/share/themes/Windows-10
         cp -r deadbeef-windows-deps/Windows-10-Icons bin/debug/share/icons/Windows-10-Icons
