61ce5c4becdc341cb2c87ae4c9f41ebb27129375
Alexey Yakovenko <wakeroid@gmail.com>
2020-09-20 11:41:38 +0200

    cleanup 18.04 docker build scripts

--- tools/docker/docker-build-clang.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker build -t deadbeef-clang -f tools/docker/Dockerfile-clang . || exit 1
-mkdir -p docker-artifacts
-docker run --rm -v ${PWD}/docker-artifacts:/usr/src/deadbeef/portable deadbeef-clang || exit 1
