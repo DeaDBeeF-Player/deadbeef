6b37d14f319066d13fa5b2730e7e5afa5d816696
Alexey Yakovenko <wakeroid@gmail.com>
2020-10-11 21:39:34 +0200

    medialib: sidecar metadata association with item names

--- metadata/metadata.h
+++ metadata/metadata.h
@@ -31,10 +31,19 @@ typedef struct ddb_keyValueIoOperation_s {
     ddb_keyValue_t *keyValue;
 } ddb_keyValueIoOperation_t;
 
+enum {
+    DDB_KEYVALUE_HASH_SIZE = 1024
+};
+
+typedef struct ddb_keyValueHashItem_s {
+    struct ddb_keyValueHashItem_s *next;
+    const char *name; // expected to be composed of Album:Artist:Dics:Track:Title
+    ddb_keyValue_t *keyValues;
+} ddb_keyValueHashItem_t;
+
 /// Thread-safe unordered set of key-values
 typedef struct {
-    /// keyvalue list
-    ddb_keyValue_t *head; // only access in data_queue
+    ddb_keyValueHashItem_t *itemHash[DDB_KEYVALUE_HASH_SIZE]; // only access in data_queue
 
     /// IO Queue
     char *filename;
@@ -61,47 +70,47 @@ void
 md_free (ddb_keyValueList_t *md);
 
 void
-md_add_with_size (ddb_keyValueList_t *md, const char *key, const char *value, size_t valuesize);
+md_add_with_size (ddb_keyValueList_t *md, const char *itemName, const char *key, const char *value, size_t valuesize);
 
 void
-md_append_value (ddb_keyValueList_t *md, const char *key, const char *value);
+md_append_value (ddb_keyValueList_t *md, const char *itemName, const char *key, const char *value);
 
 void
-md_append_with_size (ddb_keyValueList_t *md, const char *key, const char *value, size_t valuesize);
+md_append_with_size (ddb_keyValueList_t *md, const char *itemName, const char *key, const char *value, size_t valuesize);
 
 /// Returns `value` if found, otherwise NULL
 const char *
-md_find_value (ddb_keyValueList_t *md, const char *key, char *value, size_t valuesize);
+md_find_value (ddb_keyValueList_t *md, const char *itemName, const char *key, char *value, size_t valuesize);
 
 int
-md_find_value_int (ddb_keyValueList_t *md, const char *key, int def);
+md_find_value_int (ddb_keyValueList_t *md, const char *itemName, const char *key, int def);
 
 int64_t
-md_find_value_int64 (ddb_keyValueList_t *md, const char *key, int64_t def);
+md_find_value_int64 (ddb_keyValueList_t *md, const char *itemName, const char *key, int64_t def);
 
 float
-md_find_value_float (ddb_keyValueList_t *md, const char *key, float def);
+md_find_value_float (ddb_keyValueList_t *md, const char *itemName, const char *key, float def);
 
 // FIXME: need a version of this method with valuesize
 void
-md_replace_value (ddb_keyValueList_t *md, const char *key, const char *value);
+md_replace_value (ddb_keyValueList_t *md, const char *itemName, const char *key, const char *value);
 
 void
-md_set_value_int (ddb_keyValueList_t *md, const char *key, int value);
+md_set_value_int (ddb_keyValueList_t *md, const char *itemName, const char *key, int value);
 
 void
-md_set_value_int64 (ddb_keyValueList_t *md, const char *key, int64_t value);
+md_set_value_int64 (ddb_keyValueList_t *md, const char *itemName, const char *key, int64_t value);
 
 void
-md_set_value_float (ddb_keyValueList_t *md, const char *key, float value);
+md_set_value_float (ddb_keyValueList_t *md, const char *itemName, const char *key, float value);
 
 void
-md_delete_value (ddb_keyValueList_t *md, const char *key);
+md_delete_value (ddb_keyValueList_t *md, const char *itemName, const char *key);
 
 void
 md_delete_all_values (ddb_keyValueList_t *md);
 
 int
-md_value_exists (ddb_keyValueList_t *md, const char *key);
+md_value_exists (ddb_keyValueList_t *md, const char *itemName, const char *key);
 
 #endif /* metadata_h */
