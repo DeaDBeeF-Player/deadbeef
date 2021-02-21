4d43003f1da75bd07b8710b1b3d665d2a9fb83bc
Alexey Yakovenko <wakeroid@gmail.com>
2021-02-21 22:56:37 +0100

    cocoaui: added spectrum analyzer; sync design mode menu item

---
+++ plugins/cocoaui/DesignMode/SpectrumAnalyzerWidget.h
@@ -0,0 +1,17 @@
+//
+//  SpectrumAnalyzerWidget.h
+//  deadbeef
+//
+//  Created by Alexey Yakovenko on 21/02/2021.
+//  Copyright © 2021 Alexey Yakovenko. All rights reserved.
+//
+
+#import "WidgetBase.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SpectrumAnalyzerWidget : WidgetBase
+
+@end
+
+NS_ASSUME_NONNULL_END
