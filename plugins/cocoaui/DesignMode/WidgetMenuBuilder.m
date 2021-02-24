37d5fea74ac203d47ee864a54c09fdd039a14e71
Alexey Yakovenko <wakeroid@gmail.com>
2021-02-24 21:15:50 +0100

    cocoaui: design mode cut, copy, paste

--- plugins/cocoaui/DesignMode/WidgetMenuBuilder.m
+++ plugins/cocoaui/DesignMode/WidgetMenuBuilder.m
@@ -9,6 +9,8 @@
 #import "DesignModeDeps.h"
 #import "WidgetMenuBuilder.h"
 
+static NSPasteboardType const ddbWidgetUTIType = @"org.deadbeef.widget";
+
 @interface WidgetMenuBuilder()
 
 @property (nonatomic,weak) id<DesignModeDepsProtocol> deps;
@@ -87,8 +89,8 @@
         if ([type isEqualToString:@"Placeholder"]) {
             continue;
         }
-        NSString *displayName = [self.deps.factory displayNameForType:type];
-        NSMenuItem *item = [[NSMenuItem alloc] initWithTitle:displayName action:nil keyEquivalent:@""];
+        NSString *typeDisplayName = [self.deps.factory displayNameForType:type];
+        NSMenuItem *item = [[NSMenuItem alloc] initWithTitle:typeDisplayName action:nil keyEquivalent:@""];
         item.target = self;
         item.action = @selector(createWidget:);
         item.representedObject = type;
@@ -97,17 +99,29 @@
 
     [menu addItem: NSMenuItem.separatorItem];
 
-    if (!isPlaceholder) {
-        NSMenuItem *itemDelete = [[NSMenuItem alloc] initWithTitle:@"Delete" action:nil keyEquivalent:@""];
-        itemDelete.target = self;
-        itemDelete.action = @selector(deleteWidget:);
-        [menu addItem: itemDelete];
-        NSMenuItem *itemCut = [[NSMenuItem alloc] initWithTitle:@"Cut" action:nil keyEquivalent:@""];
-        [menu addItem: itemCut];
-        NSMenuItem *itemCopy = [[NSMenuItem alloc] initWithTitle:@"Copy" action:nil keyEquivalent:@""];
-        [menu addItem: itemCopy];
+    NSMenuItem *itemDelete = [[NSMenuItem alloc] initWithTitle:@"Delete" action:@selector(deleteWidget:) keyEquivalent:@""];
+    itemDelete.target = self;
+    [menu addItem: itemDelete];
+
+    NSMenuItem *itemCut = [[NSMenuItem alloc] initWithTitle:@"Cut" action:@selector(cutWidget:) keyEquivalent:@""];
+    itemCut.enabled = !isPlaceholder;
+    itemCut.target = self;
+    [menu addItem: itemCut];
+
+    NSMenuItem *itemCopy = [[NSMenuItem alloc] initWithTitle:@"Copy" action:@selector(copyWidget:) keyEquivalent:@""];
+    itemCut.enabled = !isPlaceholder;
+    itemCopy.target = self;
+    [menu addItem: itemCopy];
+
+    NSMenuItem *itemPaste = [[NSMenuItem alloc] initWithTitle:@"Paste" action:@selector(pasteWidget:) keyEquivalent:@""];
+    itemPaste.target = self;
+
+    NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
+    BOOL hasClipboard = [pasteboard canReadItemWithDataConformingToTypes:@[ddbWidgetUTIType]];
+    if (!hasClipboard) {
+        itemPaste.enabled = NO;
     }
-    NSMenuItem *itemPaste = [[NSMenuItem alloc] initWithTitle:@"Paste" action:nil keyEquivalent:@""];
+
     [menu addItem: itemPaste];
 
     // TODO: Widget custom menu (options)
@@ -116,12 +130,8 @@
     return menu;
 }
 
-- (void)createWidget:(NSMenuItem *)sender {
-    NSString *type = sender.representedObject;
-    id<WidgetProtocol> widget = [self.deps.factory createWidgetWithType:type];
-
+- (void)replaceActiveWidgetWith:(id<WidgetProtocol>)widget {
     id<WidgetProtocol> activeWidget = self.activeWidget;
-
     if (activeWidget.parentWidget == nil) {
         [activeWidget removeChild:activeWidget.childWidgets.firstObject];
         [activeWidget appendChild:widget];
@@ -136,6 +146,14 @@
     [self.deps.state layoutDidChange];
 }
 
+- (void)createWidget:(NSMenuItem *)sender {
+    NSString *type = sender.representedObject;
+    id<WidgetProtocol> widget = [self.deps.factory createWidgetWithType:type];
+
+
+    [self replaceActiveWidgetWith:widget];
+}
+
 - (void)deleteWidget:(NSMenuItem *)sender {
     id<WidgetProtocol> activeWidget = self.activeWidget;
     id<WidgetProtocol> parentWidget = activeWidget.parentWidget;
@@ -143,6 +161,42 @@
     if (activeWidget != nil && parentWidget != nil) {
         [parentWidget removeChild:activeWidget];
         [self.deps.state layoutDidChange];
+    }
+}
+
+- (void)cutWidget:(NSMenuItem *)sender {
+    [self copyWidget:sender];
+    [self deleteWidget:sender];
+}
+
+- (void)copyWidget:(NSMenuItem *)sender {
+    id<WidgetProtocol> activeWidget = self.activeWidget;
+
+    NSDictionary *widgetDict = [self.deps.serializer saveWidgetToDictionary:activeWidget];
+    NSPasteboard *pasteboard = NSPasteboard.generalPasteboard;
+    [pasteboard clearContents];
+    NSData *data = [NSKeyedArchiver archivedDataWithRootObject:widgetDict];
+    [pasteboard setData:data forType:ddbWidgetUTIType];
+}
+
+- (void)pasteWidget:(NSMenuItem *)sender {
+    NSPasteboard *pasteboard = NSPasteboard.generalPasteboard;
+    if ([pasteboard canReadItemWithDataConformingToTypes:@[ddbWidgetUTIType]]) {
+        NSData *data = [pasteboard dataForType:ddbWidgetUTIType];
+        if (!data) {
+            return;
+        }
+        NSDictionary *widgetDict = (NSDictionary *)[NSKeyedUnarchiver unarchiveObjectWithData: data];
+        if (!widgetDict) {
+            return;
+        }
+
+        id<WidgetProtocol> widget = [self.deps.serializer loadFromDictionary:widgetDict];
+        if (!widget) {
+            return;
+        }
+
+        [self replaceActiveWidgetWith:widget];
     }
 }
 
