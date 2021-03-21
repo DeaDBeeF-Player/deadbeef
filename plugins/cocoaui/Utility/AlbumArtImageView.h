c3a3ccf454632e2def0b0e5ca7820b470176734f
Alexey Yakovenko <wakeroid@gmail.com>
2021-03-21 20:44:38 +0100

    cocoaui: added album art widget; some cleanup improvements in design mode

---
+++ plugins/cocoaui/Utility/AlbumArtImageView.h
@@ -0,0 +1,18 @@
+//
+//  AlbumArtImageView.h
+//  ddbcore
+//
+//  Created by Alexey Yakovenko on 21/03/2021.
+//  Copyright © 2021 Alexey Yakovenko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// A wrapper to allow the NSImageView to shrink to smaller size than the image size
+@interface AlbumArtImageView : NSImageView
+
+@end
+
+NS_ASSUME_NONNULL_END
