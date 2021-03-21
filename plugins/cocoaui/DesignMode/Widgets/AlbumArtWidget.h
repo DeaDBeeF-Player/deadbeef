11784cab341f77f5e40ca6cb61ce9a19e46d24dc
Alexey Yakovenko <wakeroid@gmail.com>
2021-03-21 11:58:57 +0100

    cocoaui: album art widget boilerplate

---
+++ plugins/cocoaui/DesignMode/Widgets/AlbumArtWidget.h
@@ -0,0 +1,17 @@
+//
+//  AlbumArtWidget.h
+//  deadbeef
+//
+//  Created by Alexey Yakovenko on 21/03/2021.
+//  Copyright © 2021 Alexey Yakovenko. All rights reserved.
+//
+
+#import "WidgetBase.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AlbumArtWidget : WidgetBase
+
+@end
+
+NS_ASSUME_NONNULL_END
