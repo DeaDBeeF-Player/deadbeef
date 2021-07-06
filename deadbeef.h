00a3b79e7eaaffa607d49241333a34f53bd97999
Alexey Yakovenko <wakeroid@gmail.com>
2021-07-06 22:04:27 +0200

    deadbeef: fixed typos in deadbeef.h

--- deadbeef.h
+++ deadbeef.h
@@ -1155,7 +1155,7 @@ typedef struct {
     int (*junk_apev2_write) (FILE *fp, DB_apev2_tag_t *tag, int write_header, int write_footer);
 
     // Returns an offset to the audio packets, after ID3v2 and APEv2 tags.
-    // Only positive values or can be returned.
+    // Only the values >=0 can be returned.
     // The position is relative to the current file offset, at the time of the call.
     int (*junk_get_leading_size) (DB_FILE *fp);
     int (*junk_get_leading_size_stdio) (FILE *fp);
