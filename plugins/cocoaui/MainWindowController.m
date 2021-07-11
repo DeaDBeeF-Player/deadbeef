1f62695daaa58508551da6b02580963a4c874a18
Alexey Yakovenko <wakeroid@gmail.com>
2021-07-11 12:01:48 +0200

    cocoaui: extract TrackPositionFormatter into a separate file

--- plugins/cocoaui/MainWindowController.m
+++ plugins/cocoaui/MainWindowController.m
@@ -27,52 +27,11 @@
 #import "MainWindowController.h"
 #import "PlaylistViewController.h"
 #import "PreferencesWindowController.h"
+#import "TrackPositionFormatter.h"
 #include "deadbeef.h"
 #include <sys/time.h>
 
 extern DB_functions_t *deadbeef;
-
-@interface TrackPositionFormatter : NSFormatter
-
-- (NSString *)stringForObjectValue:(NSControl *)obj;
-
-- (BOOL)getObjectValue:(out id  _Nullable *)obj forString:(NSString *)string errorDescription:(out NSString * _Nullable *)error;
-
-@end
-
-@implementation TrackPositionFormatter
-
-- (NSString *)stringForObjectValue:(NSControl *)obj {
-
-    DB_playItem_t *track = deadbeef->streamer_get_playing_track ();
-    if (!track) {
-        return @"--:--:--";
-    }
-
-    double pos = obj.doubleValue / 100;
-    float duration = 0;
-    duration = deadbeef->pl_get_item_duration (track);
-    double time = duration * pos;
-    int hr = time/3600;
-    int mn = (time-hr*3600)/60;
-    int sc = round(time-hr*3600-mn*60);
-
-    NSString *res = [NSString stringWithFormat:@"%02d:%02d:%02d", hr, mn, sc];
-
-    deadbeef->pl_item_unref (track);
-
-    return res;
-}
-
-- (BOOL)getObjectValue:(out id  _Nullable __autoreleasing *)obj forString:(NSString *)string errorDescription:(out NSString * _Nullable __autoreleasing *)error {
-    *error = @"error";
-    return NO;
-}
-
-@end
-
-#pragma mark -
-
 
 @interface MainWindowController () {
     NSTimer *_updateTimer;
