205a24f7259acd2c0293238d844aaccde4926a50
Alexey Yakovenko <wakeroid@gmail.com>
2021-07-24 12:37:43 +0200

    viz: use Accelerate for fft calculation on mac

--- fft.h
+++ fft.h
@@ -1,25 +1,33 @@
 /*
- * fft.h
- * Copyright 2011 John Lindgren
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions, and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions, and the following disclaimer in the documentation
- *    provided with the distribution.
- *
- * This software is provided "as is" and without any warranty, express or
- * implied. In no event shall the authors be liable for any damages arising from
- * the use of this software.
- */
+    DeaDBeeF -- the music player
+    Copyright (C) 2009-2021 Alexey Yakovenko and other contributors
 
-#ifndef AUDACIOUS_FFT_H
-#define AUDACIOUS_FFT_H
+    This software is provided 'as-is', without any express or implied
+    warranty.  In no event will the authors be held liable for any damages
+    arising from the use of this software.
 
-void calc_freq (const float data[512], float freq[256]);
+    Permission is granted to anyone to use this software for any purpose,
+    including commercial applications, and to alter it and redistribute it
+    freely, subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+
+    2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+
+    3. This notice may not be removed or altered from any source distribution.
+*/
+
+#ifndef FFT_H
+#define FFT_H
+
+void
+fft_calculate (const float *data, float *freq);
+
+void
+fft_free (void);
 
 #endif
