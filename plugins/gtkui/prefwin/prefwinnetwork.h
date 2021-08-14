81d61475199eccb23d3938e81312dd909d5624e4
Alexey Yakovenko <wakeroid@gmail.com>
2021-08-14 19:50:01 +0200

    gtkui: extract prefwin network and appearance tabs code

--- plugins/gtkui/prefwin/prefwinnetwork.h
+++ plugins/gtkui/prefwin/prefwinnetwork.h
@@ -20,40 +20,11 @@
 
     3. This notice may not be removed or altered from any source distribution.
 */
-#ifndef prefwin_h
-#define prefwin_h
 
-extern int PREFWIN_TAB_INDEX_SOUND;
-extern int PREFWIN_TAB_INDEX_PLAYBACK;
-extern int PREFWIN_TAB_INDEX_DSP;
-extern int PREFWIN_TAB_INDEX_GUI;
-extern int PREFWIN_TAB_INDEX_APPEARANCE;
-extern int PREFWIN_TAB_INDEX_MEDIALIB;
-extern int PREFWIN_TAB_INDEX_NETWORK;
-extern int PREFWIN_TAB_INDEX_HOTKEYS;
-extern int PREFWIN_TAB_INDEX_PLUGINS;
-
-/// Run the Preferences window.
-/// @param tab_index The page index to activate, or -1 to keep the previous one active.
-void
-prefwin_run (int tab_index);
+#ifndef prefwinnetwork_h
+#define prefwinnetwork_h
 
 void
-prefwin_fill_soundcards (void);
+prefwin_init_network_tab (GtkWidget *prefwin);
 
-void
-prefwin_set_scale (const char *scale_name, int value);
-
-void
-prefwin_set_combobox (GtkComboBox *combo, int i);
-
-void
-prefwin_set_toggle_button (const char *button_name, int value);
-
-void
-prefwin_set_entry_text (const char *entry_name, const char *text);
-
-void
-prefwin_init_theme_colors (void);
-
-#endif /* prefwin_h */
+#endif /* prefwinnetwork_h */
