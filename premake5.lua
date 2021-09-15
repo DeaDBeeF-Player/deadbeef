a9415cc3f4a7e778fbc7858aab2072b701388305
Jakub Wasylków <kuba_160@protonmail.com>
2021-09-15 20:33:06 +0200

    premake: medialib build fix

--- premake5.lua
+++ premake5.lua
@@ -30,7 +30,7 @@ if _OPTIONS["standard"] ~= nil then
   plugins_to_disable = {"plugin-converter", "plugin-converter_gtk2",
                         "plugin-converter_gtk3","plugin-ffmpeg","plugin-waveout",
                         "plugin-wildmidi", "plugin-soundtouch", "plugin-sid", "plugin-gme",
-                        "plugin-mms", "plugin-cdda", "plugin-sc68", "plugin-vtx", "plugin-medialib"}
+                        "plugin-mms", "plugin-cdda", "plugin-sc68", "plugin-vtx"}
   for i,v in ipairs(plugins_to_disable) do
     if _OPTIONS[v] == nil then
       _OPTIONS[v] = "disabled"
@@ -1169,6 +1169,8 @@ project "medialib"
     "plugins/medialib/medialib.c"
   }
   pkgconfig ("jansson")
+  buildoptions {"-fblocks"}
+  links {"dispatch", "BlocksRuntime"}
 end
 
 project "translations"
