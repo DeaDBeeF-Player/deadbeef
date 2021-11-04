93f84cbaed2945dfc70bce3a03842fb314a0c273
Alexey Yakovenko <wakeroid@gmail.com>
2021-11-04 22:55:00 +0100

    cocoaui: switch scope rendering to 30fps, render on background thread

--- plugins/cocoaui/Visualization/Scope/AAPLConfig.h
+++ plugins/cocoaui/Visualization/Scope/AAPLConfig.h
@@ -11,20 +11,14 @@ Header defining preprocessor conditional values that control the configuration o
 // When disabled, rendering occurs on a background thread, allowing
 // the UI to respond more quickly in some cases because events can be 
 // processed asynchronously from potentially CPU-intensive rendering code.
-#define RENDER_ON_MAIN_THREAD 1
+#define RENDER_ON_MAIN_THREAD 0
 
 // When enabled, the view continually animates and renders
 // frames 60 times a second.  When disabled, rendering is event
 // based, occurring when a UI event requests a redraw.
-#define ANIMATION_RENDERING   1
+#define ANIMATION_RENDERING   0
 
 // When enabled, the drawable's size is updated automatically whenever
 // the view is resized. When disabled, you can update the drawable's
 // size explicitly outside the view class.
 #define AUTOMATICALLY_RESIZE  1
-
-// When enabled, the renderer creates a depth target (i.e. depth buffer)
-// and attaches with the render pass descritpr along with the drawable
-// texture for rendering.  This enables the app properly perform depth testing.
-#define CREATE_DEPTH_BUFFER   1
-
