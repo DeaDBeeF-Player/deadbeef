DeaDBeeF-Player/deadbeef

commit 5e0a8f1f67ca1d185afe4a7dea2bdbe03aec42df

insert and delete keys processing in track properties

plugins/gtkui/trkproperties.c
@@ -6,12 +6,12 @@
     modify it under the terms of the GNU General Public License
     as published by the Free Software Foundation; either version 2
     of the License, or (at your option) any later version.
-    
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-    
+
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -190,6 +190,14 @@ on_trackproperties_key_press_event     (GtkWidget       *widget,
 {
     if (event->keyval == GDK_Escape) {
         on_trackproperties_delete_event (trackproperties, NULL, NULL);
+        return TRUE;
+    }
+    else if (event->keyval == GDK_Delete) {
+        on_remove_field_activate (NULL, NULL);
+        return TRUE;
+    }
+    else if (event->keyval == GDK_Insert) {
+        on_add_field_activate (NULL, NULL);
         return TRUE;
     }
     return FALSE;
@@ -626,6 +634,10 @@ on_write_tags_clicked                  (GtkButton       *button,
 void
 on_add_field_activate                 (GtkMenuItem     *menuitem,
                                         gpointer         user_data) {
+    GtkTreeView *treeview = GTK_TREE_VIEW (lookup_widget (trackproperties, "metalist"));
+    if (!gtk_widget_is_focus(treeview)) {
+        return; // do not add field if Metadata tab is not focused
+    }
     GtkWidget *dlg = create_entrydialog ();
     gtk_dialog_set_default_response (GTK_DIALOG (dlg), GTK_RESPONSE_OK);
     gtk_window_set_title (GTK_WINDOW (dlg), _("Edit playlist"));
@@ -636,9 +648,9 @@ on_add_field_activate                 (GtkMenuItem     *menuitem,
         int res = gtk_dialog_run (GTK_DIALOG (dlg));
         if (res == GTK_RESPONSE_OK) {
             e = lookup_widget (dlg, "title");
-            
+
             const char *text = gtk_entry_get_text (GTK_ENTRY(e));
-            
+
             GtkTreeIter iter;
 
             // check for _ and :
@@ -674,6 +686,11 @@ on_add_field_activate                 (GtkMenuItem     *menuitem,
 
                 gtk_list_store_append (store, &iter);
                 gtk_list_store_set (store, &iter, 0, title, 1, value, 2, key, -1);
+                GtkTreePath *path;
+                gint rows = gtk_tree_model_iter_n_children (GTK_TREE_MODEL (store), NULL);
+                path = gtk_tree_path_new_from_indices (rows - 1, -1);
+                gtk_tree_view_set_cursor (treeview, path, NULL, TRUE); // set cursur onto new field
+                gtk_tree_path_free(path);
                 trkproperties_modified = 1;
             }
             else {
@@ -694,20 +711,14 @@ void
 on_remove_field_activate                 (GtkMenuItem     *menuitem,
                                         gpointer         user_data) {
 
+    GtkTreeView *treeview = GTK_TREE_VIEW (lookup_widget (trackproperties, "metalist"));
+    if (!gtk_widget_is_focus(treeview)) {
+        return; // do not remove field if Metadata tab is not focused
+    }
     GtkTreePath *path;
     GtkTreeViewColumn *col;
-    GtkTreeView *treeview = GTK_TREE_VIEW (lookup_widget (trackproperties, "metalist"));
     gtk_tree_view_get_cursor (treeview, &path, &col);
     if (!path || !col) {
-        return;
-    }
-
-    GtkWidget *dlg = gtk_message_dialog_new (GTK_WINDOW (trackproperties), GTK_DIALOG_MODAL, GTK_MESSAGE_WARNING, GTK_BUTTONS_YES_NO, _("Really remove selected field?"));
-    gtk_window_set_title (GTK_WINDOW (dlg), _("Warning"));
-
-    int response = gtk_dialog_run (GTK_DIALOG (dlg));
-    gtk_widget_destroy (dlg);
-    if (response != GTK_RESPONSE_YES) {
         return;
     }
 
@@ -730,6 +741,7 @@ on_remove_field_activate                 (GtkMenuItem     *menuitem,
     else {
         gtk_list_store_remove (store, &iter);
     }
+    gtk_tree_view_set_cursor (treeview, path, NULL, FALSE); // restore cursur after deletion
     gtk_tree_path_free (path);
     trkproperties_modified = 1;
 }

commit 54a0f206054dfe3a488bfc143102d9c9e7f7706f

gtkui: fix a consequence of rev 5e0a8f1f67ca1d185afe4a7dea2bdbe03aec42df, which broke adding new fields in track properties (fixes #2700)

plugins/gtkui/deadbeef.glade
@@ -1183,7 +1183,7 @@
   <signal name="window_state_event" handler="on_trackproperties_window_state_event" last_modification_time="Thu, 10 Mar 2011 11:08:02 GMT"/>
 
   <child>
-    <widget class="GtkNotebook" id="notebook3">
+    <widget class="GtkNotebook" id="trkproperties_notebook">
       <property name="visible">True</property>
       <property name="can_focus">True</property>
       <property name="show_tabs">True</property>

plugins/gtkui/interface.c
@@ -1008,7 +1008,7 @@ GtkWidget*
 create_trackproperties (void)
 {
   GtkWidget *trackproperties;
-  GtkWidget *notebook3;
+  GtkWidget *trkproperties_notebook;
   GtkWidget *vbox16;
   GtkWidget *hbox109;
   GtkWidget *label136;
@@ -1051,13 +1051,13 @@ create_trackproperties (void)
   gtk_window_set_skip_taskbar_hint (GTK_WINDOW (trackproperties), TRUE);
   gtk_window_set_skip_pager_hint (GTK_WINDOW (trackproperties), TRUE);
 
-  notebook3 = gtk_notebook_new ();
-  gtk_widget_show (notebook3);
-  gtk_container_add (GTK_CONTAINER (trackproperties), notebook3);
+  trkproperties_notebook = gtk_notebook_new ();
+  gtk_widget_show (trkproperties_notebook);
+  gtk_container_add (GTK_CONTAINER (trackproperties), trkproperties_notebook);
 
   vbox16 = gtk_vbox_new (FALSE, 8);
   gtk_widget_show (vbox16);
-  gtk_container_add (GTK_CONTAINER (notebook3), vbox16);
+  gtk_container_add (GTK_CONTAINER (trkproperties_notebook), vbox16);
   gtk_container_set_border_width (GTK_CONTAINER (vbox16), 12);
 
   hbox109 = gtk_hbox_new (FALSE, 8);
@@ -1158,11 +1158,11 @@ create_trackproperties (void)
 
   label64 = gtk_label_new (_("Metadata"));
   gtk_widget_show (label64);
-  gtk_notebook_set_tab_label (GTK_NOTEBOOK (notebook3), gtk_notebook_get_nth_page (GTK_NOTEBOOK (notebook3), 0), label64);
+  gtk_notebook_set_tab_label (GTK_NOTEBOOK (trkproperties_notebook), gtk_notebook_get_nth_page (GTK_NOTEBOOK (trkproperties_notebook), 0), label64);
 
   vbox24 = gtk_vbox_new (FALSE, 8);
   gtk_widget_show (vbox24);
-  gtk_container_add (GTK_CONTAINER (notebook3), vbox24);
+  gtk_container_add (GTK_CONTAINER (trkproperties_notebook), vbox24);
   gtk_container_set_border_width (GTK_CONTAINER (vbox24), 12);
 
   scrolledwindow6 = gtk_scrolled_window_new (NULL, NULL);
@@ -1204,7 +1204,7 @@ create_trackproperties (void)
 
   label80 = gtk_label_new (_("Properties"));
   gtk_widget_show (label80);
-  gtk_notebook_set_tab_label (GTK_NOTEBOOK (notebook3), gtk_notebook_get_nth_page (GTK_NOTEBOOK (notebook3), 1), label80);
+  gtk_notebook_set_tab_label (GTK_NOTEBOOK (trkproperties_notebook), gtk_notebook_get_nth_page (GTK_NOTEBOOK (trkproperties_notebook), 1), label80);
 
   g_signal_connect ((gpointer) trackproperties, "key_press_event",
                     G_CALLBACK (on_trackproperties_key_press_event),
@@ -1236,7 +1236,7 @@ create_trackproperties (void)
 
   /* Store pointers to all widgets, for use by lookup_widget(). */
   GLADE_HOOKUP_OBJECT_NO_REF (trackproperties, trackproperties, "trackproperties");
-  GLADE_HOOKUP_OBJECT (trackproperties, notebook3, "notebook3");
+  GLADE_HOOKUP_OBJECT (trackproperties, trkproperties_notebook, "trkproperties_notebook");
   GLADE_HOOKUP_OBJECT (trackproperties, vbox16, "vbox16");
   GLADE_HOOKUP_OBJECT (trackproperties, hbox109, "hbox109");
   GLADE_HOOKUP_OBJECT (trackproperties, label136, "label136");

plugins/gtkui/trkproperties.c
@@ -911,8 +911,8 @@ on_trkproperties_add_new_field_activate
                                         (GtkMenuItem     *menuitem,
                                         gpointer         user_data)
 {
-    GtkTreeView *treeview = GTK_TREE_VIEW (lookup_widget (trackproperties, "metalist"));
-    if (!gtk_widget_is_focus(GTK_WIDGET (treeview))) {
+    GtkNotebook *notebook = GTK_NOTEBOOK(lookup_widget(trackproperties, "trkproperties_notebook"));
+    if (gtk_notebook_get_current_page(notebook) != 0) {
         return; // do not add field if Metadata tab is not focused
     }
     GtkWidget *dlg = create_entrydialog ();
@@ -922,6 +922,7 @@ on_trkproperties_add_new_field_activate
     GtkWidget *e;
     e = lookup_widget (dlg, "title_label");
     gtk_label_set_text (GTK_LABEL(e), _("Name:"));
+    GtkTreeView *treeview = GTK_TREE_VIEW (lookup_widget (trackproperties, "metalist"));
     for (;;) {
         int res = gtk_dialog_run (GTK_DIALOG (dlg));
         if (res == GTK_RESPONSE_OK) {
@@ -1008,10 +1009,12 @@ void
 on_trkproperties_crop_activate         (GtkMenuItem     *menuitem,
                                         gpointer         user_data)
 {
-    GtkTreeView *treeview = GTK_TREE_VIEW (lookup_widget (trackproperties, "metalist"));
-    if (!gtk_widget_is_focus(GTK_WIDGET (treeview))) {
+    GtkNotebook *notebook = GTK_NOTEBOOK(lookup_widget(trackproperties, "trkproperties_notebook"));
+    if (gtk_notebook_get_current_page(notebook) != 0) {
         return; // do not remove field if Metadata tab is not focused
     }
+
+    GtkTreeView *treeview = GTK_TREE_VIEW (lookup_widget (trackproperties, "metalist"));
     GtkTreePath *path;
     gtk_tree_view_get_cursor (treeview, &path, NULL);
     if (!path) {