04424fdb4d1bd10d97627158aee439a5d1d3c945
Alexey Yakovenko <wakeroid@gmail.com>
2021-12-19 20:52:22 +0100

    linux build fixes

--- premake5.lua
+++ premake5.lua
@@ -808,6 +808,7 @@ project "ddb_gui_GTK2"
     "plugins/gtkui/*.c",
     "plugins/gtkui/medialib/*.c",
     "plugins/gtkui/prefwin/*.c",
+    "plugins/gtkui/covermanager/*.c",
     "shared/eqpreset.c",
     "shared/pluginsettings.c",
     "shared/trkproperties_shared.c",
