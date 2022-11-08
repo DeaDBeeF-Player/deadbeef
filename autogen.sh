e68e2b67bcb1af95f0a1f8b16c0ef718adf4ecbc
Oleksiy Yakovenko <wakeroid@gmail.com>
2022-11-08 22:43:40 +0100

    autogen.sh simplified error handling

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-echo "Running autopoint..."; autopoint -f || :
-echo "Running aclocal..."; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
-echo "Running autoheader..."; autoheader || exit 1
-echo "Running autoconf..."; autoconf || exit 1
-echo "Running libtoolize..."; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
-echo "Running automake..."; automake --add-missing --copy --gnu || exit 1
+set -e
+echo "Running autopoint..."; autopoint -f
+echo "Running aclocal..."; aclocal $ACLOCAL_FLAGS -I m4
+echo "Running autoheader..."; autoheader
+echo "Running autoconf..."; autoconf
+echo "Running libtoolize..."; (libtoolize --copy --automake || glibtoolize --automake)
+echo "Running automake..."; automake --add-missing --copy --gnu
 echo "Running intltoolize"; intltoolize --force --automake
