4baaa6b5d6b9c4b469584b32468fef18427bc02e
Oleksiy Yakovenko <wakeroid@gmail.com>
2022-12-04 10:44:57 +0100

    undo: preserve selection for undo operations

--- undo/undo_playlist.c
+++ undo/undo_playlist.c
@@ -24,13 +24,20 @@
 #include <stdlib.h>
 #include "undo_playlist.h"
 
+#define UNDO_SELECTION_LIST_UNSELECTED 1
+
 typedef struct {
     undo_operation_t _super;
     playlist_t *plt;
     playItem_t **items;
     playItem_t *insert_position; // insert after this item
     size_t count;
+    uint32_t flags; // reusable flag fields, meaning changes depending on the operation type.
 } undo_operation_item_list_t;
+
+void
+undo_change_selection(undobuffer_t *undobuffer, playlist_t *plt);
+
 
 static void
 _undo_operation_item_list_deinit(undo_operation_item_list_t *op) {
@@ -45,15 +52,20 @@ _undo_operation_item_list_deinit(undo_operation_item_list_t *op) {
 }
 
 static undo_operation_item_list_t *
-_undo_operation_item_list_new(undobuffer_t *undobuffer, playlist_t *plt, playItem_t **items, size_t count) {
+_undo_operation_item_list_new(undobuffer_t *undobuffer, playlist_t *plt, playItem_t **items, size_t count, int copy) {
     undo_operation_item_list_t *op = calloc (1, sizeof (undo_operation_item_list_t));
-    op->items = calloc (count, sizeof (playItem_t *));
     op->count = count;
     plt_ref(plt);
     op->plt = plt;
-    for (size_t i = 0; i < count; i++) {
-        op->items[i] = items[i];
-        pl_item_ref(items[i]);
+    if (copy) {
+        op->items = calloc (count, sizeof (playItem_t *));
+        for (size_t i = 0; i < count; i++) {
+            op->items[i] = items[i];
+            pl_item_ref(items[i]);
+        }
+    }
+    else {
+        op->items = items;
     }
     op->_super.deinit = (undo_operation_deinit_fn)_undo_operation_item_list_deinit;
     return op;
@@ -66,6 +78,7 @@ _undo_perform_remove_items(undobuffer_t *undobuffer, undo_operation_item_list_t 
         plt_remove_item(op->plt, op->items[i]);
     }
 }
+
 static void
 _undo_perform_insert_items (undobuffer_t *undobuffer, undo_operation_item_list_t *op) {
     // FIXME: add plt_insert_items function
@@ -76,24 +89,93 @@ _undo_perform_insert_items (undobuffer_t *undobuffer, undo_operation_item_list_t
     }
 }
 
+static void
+_undo_perform_change_selection (undobuffer_t *undobuffer, undo_operation_item_list_t *op) {
+    // Restore selection according to the flag.
+    if (op->flags & UNDO_SELECTION_LIST_UNSELECTED) {
+        plt_select_all(op->plt);
+    }
+    else {
+        plt_deselect_all(op->plt);
+    }
+
+    for (size_t i = 0; i < op->count; i++) {
+        pl_set_selected(op->items[i], !(op->flags & UNDO_SELECTION_LIST_UNSELECTED));
+    }
+}
+
+static int
+_undo_operation_prepare(undobuffer_t *undobuffer, playlist_t *plt) {
+    if (!undobuffer_is_enabled(undobuffer)) {
+        return 1;
+    }
+
+    if (!undobuffer_has_operations(undobuffer)) {
+        undo_change_selection(undobuffer, plt);
+    }
+
+    return 0;
+}
+
 // It's required that the items don't have holes, i.e. they're linked together
 void
 undo_remove_items(undobuffer_t *undobuffer, playlist_t *plt, playItem_t **items, size_t count) {
-    if (!undobuffer_is_enabled(undobuffer)) {
+    if (_undo_operation_prepare (undobuffer, plt) != 0) {
         return;
     }
     playItem_t *after = pl_get_prev(items[0], PL_MAIN);
-    undo_operation_item_list_t *op = _undo_operation_item_list_new(undobuffer, plt, items, count);
+    undo_operation_item_list_t *op = _undo_operation_item_list_new(undobuffer, plt, items, count, 1);
     op->insert_position = after;
     op->_super.perform = (undo_operation_perform_fn)_undo_perform_insert_items;
     undobuffer_append_operation(undobuffer, &op->_super);
 }
+
 void
 undo_insert_items(undobuffer_t *undobuffer, playlist_t *plt, playItem_t **items, size_t count) {
+    if (_undo_operation_prepare (undobuffer, plt) != 0) {
+        return;
+    }
+    undo_operation_item_list_t *op = _undo_operation_item_list_new(undobuffer, plt, items, count, 1);
+    op->_super.perform = (undo_operation_perform_fn)_undo_perform_remove_items;
+    undobuffer_append_operation(undobuffer, &op->_super);
+}
+
+void
+undo_change_selection(undobuffer_t *undobuffer, playlist_t *plt) {
     if (!undobuffer_is_enabled(undobuffer)) {
         return;
     }
-    undo_operation_item_list_t *op = _undo_operation_item_list_new(undobuffer, plt, items, count);
-    op->_super.perform = (undo_operation_perform_fn)_undo_perform_remove_items;
+
+    // Capture a list of all selected or unselected items, whichever is smaller,
+    // and indicate which one is captured with a flag.
+
+    const int selcount = plt_getselcount(plt);
+    const int unselcount = plt_get_item_count(plt, PL_MAIN);
+
+    uint32_t flags = 0;
+    playItem_t **items;
+    size_t count = 0;
+    if (selcount > unselcount) {
+        flags = UNDO_SELECTION_LIST_UNSELECTED;
+        count = unselcount;
+        items = calloc (count, sizeof (playItem_t *));
+    }
+    else {
+        count = selcount;
+        items = calloc (count, sizeof (playItem_t *));
+    }
+    int index = 0;
+    const int picking_selected = (flags & UNDO_SELECTION_LIST_UNSELECTED) == 0;
+    for (playItem_t *item = plt->head[PL_MAIN]; item != NULL; item = item->next[PL_MAIN]) {
+        if (item->selected == picking_selected) {
+            items[index++] = item;
+            pl_item_ref (item);
+        }
+    }
+
+    undo_operation_item_list_t *op = _undo_operation_item_list_new(undobuffer, plt, items, count, 0);
+    op->flags = flags;
+
+    op->_super.perform = (undo_operation_perform_fn)_undo_perform_change_selection;
     undobuffer_append_operation(undobuffer, &op->_super);
 }
