0c8c4cd738cfd0552d3c35333c389c0f212f6b76
Oleksiy Yakovenko <wakeroid@gmail.com>
2023-10-16 18:40:46 +0200

    update transifex configuration

--- scripts/tx-sync.sh
+++ scripts/tx-sync.sh
@@ -7,7 +7,7 @@ cd po
 PATH="/usr/local/opt/gettext/bin:$PATH" make update-po
 cd ..
 tx push -s
-tx pull -a -f --no-interactive
+tx pull -a -f
 
 for i in po/*.po ; do
     if ! [[ $(git diff $i | grep '+msgstr') ]]; then
