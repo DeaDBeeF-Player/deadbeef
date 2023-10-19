50cbb94f83a57a874dddb57a88f8392139f81b5a
Oleksiy Yakovenko <wakeroid@gmail.com>
2023-10-19 19:00:15 +0200

    notify: cleanup importing legacy titleformat config

--- plugins/notify/notify.c
+++ plugins/notify/notify.c
@@ -323,24 +323,9 @@ notify_message (uint32_t id, uintptr_t ctx, uint32_t p1, uint32_t p2) {
     return 0;
 }
 
-static void
-import_legacy_tf (const char *key_from, const char *key_to) {
-    deadbeef->conf_lock ();
-    if (!deadbeef->conf_get_str_fast (key_to, NULL)
-            && deadbeef->conf_get_str_fast (key_from, NULL)) {
-        char old[200], new[200];
-        deadbeef->conf_get_str (key_from, "", old, sizeof (old));
-        deadbeef->tf_import_legacy (old, new, sizeof (new));
-        deadbeef->conf_set_str (key_to, new);
-    }
-    deadbeef->conf_unlock ();
-}
-
 int
 notify_start (void) {
     queue = dispatch_queue_create("OSDNotifyQueue", NULL);
-    import_legacy_tf ("notify.format", "notify.format_title_tf");
-    import_legacy_tf ("notify.format_content", "notify.format_content_tf");
     return 0;
 }
 
