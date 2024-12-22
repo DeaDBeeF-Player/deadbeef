d80e6ccefbda8810647506cedd4f6dbcb9065545
Oleksiy Yakovenko <wakeroid@gmail.com>
2024-12-22 12:27:40 +0100

    Run docker scripts in rosetta mode on mac

--- tools/docker/18.04/build.sh
+++ tools/docker/18.04/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build --progress plain -t deadbeef-clang-18.04 -f tools/docker/18.04/Dockerfile . || exit 1
+docker build --platform linux/amd64 --progress plain -t deadbeef-clang-18.04 -f tools/docker/18.04/Dockerfile . || exit 1
 mkdir -p docker-artifacts
-docker run --rm -v ${PWD}/docker-artifacts:/usr/src/deadbeef/portable deadbeef-clang-18.04 || exit 1
+docker run --platform linux/amd64 --rm -v ${PWD}/docker-artifacts:/usr/src/deadbeef/portable deadbeef-clang-18.04 || exit 1
--- tools/docker/18.04/test.sh
+++ tools/docker/18.04/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build --progress plain -t deadbeef-test-18.04 -f tools/docker/18.04/Dockerfile-test . || exit 1
-docker run --rm deadbeef-test-18.04 || exit 1
+docker build --platform linux/amd64 --progress plain -t deadbeef-test-18.04 -f tools/docker/18.04/Dockerfile-test . || exit 1
+docker run --platform linux/amd64 --rm deadbeef-test-18.04 || exit 1
 
