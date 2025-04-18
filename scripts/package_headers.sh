b06080f93ab3c511a10fe840e5505fff2afe8175
Oleksiy Yakovenko <wakeroid@gmail.com>
2025-04-18 22:10:26 +0200

    update scripts/package_headers.sh

--- scripts/package_headers.sh
+++ scripts/package_headers.sh
@@ -3,10 +3,11 @@
 # Create ddb-headers-latest.tar.bz2 in the current folder
 
 mkdir -p /tmp/deadbeef-headers/deadbeef
-cp deadbeef.h plugins/gtkui/gtkui_api.h /tmp/deadbeef-headers/deadbeef/
+cp include/deadbeef/deadbeef.h plugins/gtkui/gtkui_api.h /tmp/deadbeef-headers/deadbeef/
 cd /tmp/deadbeef-headers
 tar jcvf ddb-headers-latest.tar.bz2 deadbeef
 cd -
 pwd
 mv /tmp/deadbeef-headers/ddb-headers-latest.tar.bz2 ./
 rm -rf /tmp/deadbeef-headers
+echo Ready: ddb-headers-latest.tar.bz2
