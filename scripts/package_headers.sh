a90c16abcf1bd4a10d2398e7683a22f9830afdcf
Oleksiy Yakovenko <wakeroid@gmail.com>
2025-04-19 02:18:42 +0200

    Include artwork.h in headers package

--- scripts/package_headers.sh
+++ scripts/package_headers.sh
@@ -3,7 +3,7 @@
 # Create ddb-headers-latest.tar.bz2 in the current folder
 
 mkdir -p /tmp/deadbeef-headers/deadbeef
-cp include/deadbeef/deadbeef.h plugins/gtkui/gtkui_api.h /tmp/deadbeef-headers/deadbeef/
+cp include/deadbeef/deadbeef.h plugins/gtkui/gtkui_api.h plugins/artwork/artwork.h /tmp/deadbeef-headers/deadbeef/
 cd /tmp/deadbeef-headers
 tar jcvf ddb-headers-latest.tar.bz2 deadbeef
 cd -
