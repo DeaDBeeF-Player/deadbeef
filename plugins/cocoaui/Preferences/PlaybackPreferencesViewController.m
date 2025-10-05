7e53a06b024f4d3bcef8f925f7e7d4ddd2dbdc2f
Oleksiy Yakovenko <wakeroid@gmail.com>
2025-10-05 09:44:21 +0200

    cocoaui: Add Always resume session in paused state (fixes #3222)

--- plugins/cocoaui/Preferences/PlaybackPreferencesViewController.m
+++ plugins/cocoaui/Preferences/PlaybackPreferencesViewController.m
@@ -27,6 +27,7 @@ extern DB_functions_t *deadbeef;
 @property (nonatomic) BOOL cliAddToSpecificPlaylist;
 @property (nonatomic) NSString *cliSpecificPlaylist;
 @property (nonatomic) BOOL resumeLastSession;
+@property (nonatomic) BOOL alwaysResumeSessionPaused;
 @property (nonatomic) BOOL ignoreArchives;
 @property (nonatomic) BOOL stopAfterCurrentReset;
 @property (nonatomic) BOOL stopAfterCurrentAlbumReset;
@@ -72,6 +73,7 @@ extern DB_functions_t *deadbeef;
     _cliAddToSpecificPlaylist =  deadbeef->conf_get_int ("cli_add_to_specific_playlist", 1) ? YES : NO;
     _cliSpecificPlaylist = conf_get_nsstr ("cli_add_playlist_name", "Default");
     _resumeLastSession = deadbeef->conf_get_int ("resume_last_session", 1) ? YES : NO;
+    _alwaysResumeSessionPaused = deadbeef->conf_get_int ("resume_always_paused", 0) ? YES : NO;
     _ignoreArchives = deadbeef->conf_get_int ("ignore_archives", 1) ? YES : NO;
     _stopAfterCurrentReset = deadbeef->conf_get_int ("playlist.stop_after_current_reset", 0) ? YES : NO;
     _stopAfterCurrentAlbumReset = deadbeef->conf_get_int ("playlist.stop_after_album_reset", 0) ? YES : NO;
@@ -157,6 +159,12 @@ extern DB_functions_t *deadbeef;
 - (void)setResumeLastSession:(BOOL)resumeLastSession {
     _resumeLastSession = resumeLastSession;
     deadbeef->conf_set_int ("resume_last_session", resumeLastSession);
+    deadbeef->sendmessage (DB_EV_CONFIGCHANGED, 0, 0, 0);
+}
+
+- (void)setAlwaysResumeSessionPaused:(BOOL)alwaysResumeSessionPaused {
+    _alwaysResumeSessionPaused = alwaysResumeSessionPaused;
+    deadbeef->conf_set_int("resume_always_paused", alwaysResumeSessionPaused);
     deadbeef->sendmessage (DB_EV_CONFIGCHANGED, 0, 0, 0);
 }
 
