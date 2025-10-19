47b6507685e265138a33f4c117449f2f1061b6c7
Oleksiy Yakovenko <wakeroid@gmail.com>
2025-10-19 21:50:16 +0200

    github: upload mac build logs on test failure

--- travis/build.sh
+++ travis/build.sh
@@ -39,7 +39,7 @@ case "$TRAVIS_OS_NAME" in
         echo "Build & run tests ..."
         mkdir -p osx/build/reports
         mkdir -p osx/build/Release
-        xcodebuild "MACOSX_DEPLOYMENT_TARGET=10.13" test -project osx/deadbeef.xcodeproj -scheme deadbeef -configuration Release | xcbeautify --report junit --report-path "osx/build/reports"  ; test ${PIPESTATUS[0]} -eq 0
+        xcodebuild "MACOSX_DEPLOYMENT_TARGET=10.13" test -project osx/deadbeef.xcodeproj -scheme deadbeef -configuration Release | tee osx/build/Release/test.log | xcbeautify --report junit --report-path "osx/build/reports"  ; test ${PIPESTATUS[0]} -eq 0
         echo "Build DeaDBeeF.app ..."
         xcodebuild "MACOSX_DEPLOYMENT_TARGET=10.13" -project osx/deadbeef.xcodeproj -target DeaDBeeF -configuration Release | tee osx/build/Release/build.log | xcbeautify ; test ${PIPESTATUS[0]} -eq 0
         cd osx/build/Release
