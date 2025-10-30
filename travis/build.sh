152560416d34ad3d959277a7c047eeb69e8241d3
Oleksiy Yakovenko <wakeroid@gmail.com>
2025-10-30 23:25:42 +0100

    wip

--- travis/build.sh
+++ travis/build.sh
@@ -39,9 +39,9 @@ case "$TRAVIS_OS_NAME" in
         echo "Build & run tests ..."
         mkdir -p osx/build/reports
         mkdir -p osx/build/Release
-        launchctl asuser $(id -u) xcodebuild "MACOSX_DEPLOYMENT_TARGET=10.13" test -project osx/deadbeef.xcodeproj -scheme deadbeef -configuration Release | tee osx/build/Release/test.log | xcbeautify --report junit --report-path "osx/build/reports"  ; test ${PIPESTATUS[0]} -eq 0
+        xcodebuild "MACOSX_DEPLOYMENT_TARGET=11.0" test -project osx/deadbeef.xcodeproj -scheme deadbeef -configuration Release | tee osx/build/Release/test.log | xcbeautify --report junit --report-path "osx/build/reports"  ; test ${PIPESTATUS[0]} -eq 0
         echo "Build DeaDBeeF.app ..."
-        launchctl asuser $(id -u) xcodebuild "MACOSX_DEPLOYMENT_TARGET=10.13" -project osx/deadbeef.xcodeproj -target DeaDBeeF -configuration Release | tee osx/build/Release/build.log | xcbeautify ; test ${PIPESTATUS[0]} -eq 0
+        xcodebuild "MACOSX_DEPLOYMENT_TARGET=11.0" -project osx/deadbeef.xcodeproj -target DeaDBeeF -configuration Release | tee osx/build/Release/build.log | xcbeautify ; test ${PIPESTATUS[0]} -eq 0
         cd osx/build/Release
         zip -r deadbeef-$VERSION-macos-universal.zip DeaDBeeF.app
         zip -r deadbeef-$VERSION-macos-dSYM.zip *.dSYM
