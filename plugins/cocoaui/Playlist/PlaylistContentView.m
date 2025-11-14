DeaDBeeF-Player/deadbeef

commit 654803ff01b8bb9c1ebab0f5e3905240fa2ec161

cocoaui: fix album art position for pinned playlist group

plugins/cocoaui/Playlist/PlaylistContentView.m
@@ -351,6 +351,19 @@ static int grouptitleheight = 22;
     if ([self.delegate respondsToSelector:@selector(scrollChanged:)]) {
         [self.delegate scrollChanged:scrollPos];
     }
+
+    if (self.delegate.pinGroups) {
+        for (DdbListviewCol_t col = (self.delegate).firstColumn;
+             col != (self.delegate).invalidColumn;
+             col = [self.delegate nextColumn:col]) {
+
+            if ([self.delegate isAlbumArtColumn:col]) {
+                // Trigger full redraw, because album art can move with pinned group title when scrolling
+                self.needsDisplay = YES;
+                break;
+            }
+        }
+    }
 }
 
 - (void)setFrameSize:(NSSize)newSize {
@@ -460,20 +473,12 @@ static int grouptitleheight = 22;
 
         // draw album art
         int grp_next_y = grp_y + grp->height;
-        [self renderAlbumArtForGroup:grp groupIndex:groupIndex isPinnedGroup:NO nextGroupCoord:grp_next_y yPos:grp_y + title_height viewportY:dirtyRect.origin.y clipRegion:dirtyRect];
+        [self renderAlbumArtForGroup:grp groupIndex:groupIndex isPinnedGroup:(self.pinnedGroupTitleView.group == grp) nextGroupCoord:grp_next_y yPos:grp_y + title_height viewportY:dirtyRect.origin.y clipRegion:dirtyRect];
 
         idx += grp->num_items;
         grp_y += grp->height;
         groupIndex += 1;
     }
-
-//    if (pin_grp) {
-//        self.pinnedGroupTitleView.hidden = NO;
-//    }
-//    else {
-//        self.pinnedGroupTitleView.hidden = NO;
-//    }
-//        [self drawGroupTitle:pin_grp grp_y:pinnedGrpPos title_height:title_height];
 
     if (cursor_it != (self.dataModel).invalidRow) {
         [self.dataModel unrefRow:cursor_it];

plugins/cocoaui/Playlist/PlaylistViewController.m
@@ -1023,15 +1023,15 @@ artwork_listener (ddb_artwork_listener_event_t event, void *user_data, int64_t p
     CGFloat min_y = (pinned ? viewportY+lv.contentView.grouptitle_height : y) + ART_PADDING_VERT;
     CGFloat max_y = grp_next_y;
 
-    CGFloat ypos = min_y;
-    if (min_y + art_width + ART_PADDING_VERT >= max_y) {
-        ypos = max_y - art_width - ART_PADDING_VERT;
-    }
-
     NSSize size = image.size;
     NSSize desiredSize = [CoverManager.shared desiredSizeForImageSize:size availableSize:availableSize];
     CGSize drawSize = [self.view convertSizeFromBacking:desiredSize];
     
+    CGFloat ypos = min_y;
+    if (min_y + drawSize.height + ART_PADDING_VERT >= max_y) {
+        ypos = max_y - drawSize.height - ART_PADDING_VERT;
+    }
+
     if (size.width < size.height) {
         plt_col_info_t *c = &self.columns[(int)col];
         if (c->alignment == ColumnAlignmentCenter) {

commit 960cfbf7797b37310413fbc75a9a3ef7e1a07ee6

cocoaui: fix album art drawing regression in pinned group mode, regression from 654803ff01b8bb9c1ebab0f5e3905240fa2ec161 (fixes #3263)

plugins/cocoaui/Playlist/PlaylistContentView.m
@@ -461,7 +461,7 @@ static int grouptitleheight = 22;
 
         // draw album art
         int grp_next_y = grp_y + grp->height;
-        [self renderAlbumArtForGroup:grp groupIndex:groupIndex isPinnedGroup:(self.pinnedGroupTitleView.group == grp) nextGroupCoord:grp_next_y yPos:grp_y + title_height viewportY:dirtyRect.origin.y clipRegion:dirtyRect];
+        [self renderAlbumArtForGroup:grp groupIndex:groupIndex isPinnedGroup:(self.pinnedGroupTitleView.group == grp) nextGroupCoord:grp_next_y yPos:grp_y + title_height viewportY:self.enclosingScrollView.documentVisibleRect.origin.y clipRegion:dirtyRect];
 
         idx += grp->num_items;
         grp_y += grp->height;

plugins/cocoaui/Playlist/PlaylistViewController.m
@@ -1012,24 +1012,22 @@ artwork_listener (ddb_artwork_listener_event_t event, void *user_data, int64_t p
             [lv.contentView drawGroup:grp];
         }];
     }
-    if (!image) {
+    if (image == nil) {
         // FIXME: the problem here is that if the cover is not found (yet) -- it won't draw anything, but the rect is already invalidated, and will come out as background color
         return;
     }
 
     NSRect drawRect;
 
-    int art_x = x + ART_PADDING_HORZ;
-    CGFloat min_y = (pinned ? viewportY+lv.contentView.grouptitle_height : y) + ART_PADDING_VERT;
-    CGFloat max_y = grp_next_y;
-
     NSSize size = image.size;
     NSSize desiredSize = [CoverManager.shared desiredSizeForImageSize:size availableSize:availableSize];
     CGSize drawSize = [self.view convertSizeFromBacking:desiredSize];
     
-    CGFloat ypos = min_y;
-    if (min_y + drawSize.height + ART_PADDING_VERT >= max_y) {
-        ypos = max_y - drawSize.height - ART_PADDING_VERT;
+    CGFloat art_x = x + ART_PADDING_HORZ;
+    CGFloat ypos = (pinned ? viewportY + lv.contentView.grouptitle_height : y) + ART_PADDING_VERT;
+
+    if (pinned && ypos + drawSize.height + ART_PADDING_VERT >= grp_next_y) {
+        ypos = grp_next_y - drawSize.height - ART_PADDING_VERT;
     }
 
     if (size.width < size.height) {