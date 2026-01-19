531323e8da66955f788e95b31f04a2bcea89a6d5
Oleksiy Yakovenko <wakeroid@gmail.com>
2026-01-19 18:34:48 +0100

    junklib: fix buffer overflow bug when copying utf8 string without conversion into a buffer that's smaller than necessary

--- src/junklib.c
+++ src/junklib.c
@@ -552,11 +552,11 @@ ddb_iconv (const char *cs_out, const char *cs_in, char *out, int outlen, const c
     // to utf8 branch
     if (!strcasecmp (cs_out, UTF8_STR)) {
         if (!strcasecmp (cs_in, UTF8_STR)) {
-            memcpy (out, in, inlen);
-            out[inlen] = 0;
-            int valid = u8_valid (out, inlen, NULL);
+            int result = u8_strnbcpy(out, in, outlen-1);
+            out[result] = 0;
+            int valid = u8_valid (out, result, NULL);
             if (valid) {
-                len = inlen;
+                len = result;
             }
         }
         else if (!strcasecmp (cs_in, "cp1251")) {
